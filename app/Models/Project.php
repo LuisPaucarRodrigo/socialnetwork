15a4eefbfbb35834c51608ab2fbea014b674e735
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2023-12-11 17:46:40 -0500

    advance project management

---
+++ app/Models/Project.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+    protected $table = 'projects';
+    protected $fillable = [
+        'name',
+        'code',
+        'start_date',
+        'end_date',
+        'priority',
+        'description'
+    ];
+
+    public function employees()
+    {
+        return $this->belongsToMany(Employee::class,'project_employee')->withPivot('charge', 'id');
+    }
+}
---
+++ database/migrations/2023_12_10_062017_create_project_employee_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_employee', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('project_id')
+                ->nullable()
+                ->constrained('projects')
+                ->onDelete('cascade');
+            $table->foreignId('employee_id')
+                ->nullable()
+                ->constrained('employees')
+                ->onDelete('set null');
+            $table->string('charge');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_employee');
+    }
+};
