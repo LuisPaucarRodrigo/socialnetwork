2b052bc90fcb6ce2e8988ebb7a0d1478e97ac76f
JuniorMolina23 <junior.molina@tecsup.edu.pe>
2023-12-13 17:26:15 -0500

    CreateTasks1.2

--- app/Models/Tasks.php
+++ app/Models/Tasks.php
@@ -22,7 +22,7 @@ class Tasks extends Model
         'percentage' => 'nullable|integer|min:0|max:100',
         'start_date' => 'required|date',
         'end_date' => 'required|date|after_or_equal:start_date',
-        'status' => 'required|in:pendiente,en-proceso,completado',
+        'status' => 'required|in:pendiente,proceso,completado',
     ];
 
     public function project()
