18d09cd80f9d0d733226992f1f0580553b5cc977
JuniorMolina23 <junior.molina@tecsup.edu.pe>
2023-12-19 09:17:26 -0500

    Resource1.0

---
+++ resources/js/Pages/Inventory/ResourceManagement/index.vue
@@ -0,0 +1,89 @@
+<template>
+    <Head title="Proyectos" />
+    <AuthenticatedLayout>
+        <template #header>
+            Recursos
+        </template>
+
+        <div class="inline-block min-w-full overflow-hidden rounded-lg shadow">
+            <div>
+                <button @click="add_resource" type="button"
+                    class="rounded-md bg-indigo-600 px-4 py-2 text-center text-sm text-white hover:bg-indigo-500 ">
+                    + Agregar
+                </button>
+            </div>
+            <div class="talwing mt-4">
+                <table class="w-full whitespace-no-wrap">
+                    <thead>
+                        <tr
+                            class="border-b bg-gray-50 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">
+                            <th
+                                class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                                Nombre
+                            </th>
+                            <th
+                                class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                                Categoria
+                            </th>
+                            <th
+                                class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                                Cantidad
+                            </th>
+                            <th
+                                class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                                Ubicacion
+                            </th>
+                            <th
+                                class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr :key="1" class="text-gray-700">
+                            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                                <p class="text-gray-900 whitespace-no-wrap">nombre</p>
+                            </td>
+                            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                                <p class="text-gray-900 whitespace-no-wrap">nombre</p>
+                            </td>
+                            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                                <p class="text-gray-900 whitespace-no-wrap">nombre</p>
+                            </td>
+                            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                                <p class="text-gray-900 whitespace-no-wrap">nombre</p>
+                            </td>
+                            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                                <div class="flex space-x-3 justify-center">
+                                    <button type="button" @click="confirmUserDeletion(user.id)"
+                                        class="text-blue-900 whitespace-no-wrap">
+                                        <EyeIcon class="text-blue-500 h-4 w-4" />
+                                    </button>
+                                    <button type="button" @click="confirmUserDeletion(user.id)"
+                                        class="text-yellow-500 whitespace-no-wrap">
+                                        <PencilIcon class="text-yellow-500 h-4 w-4"></PencilIcon>
+                                    </button>
+                                    <button type="button" @click="confirmUserDeletion(user.id)"
+                                        class="text-red-900 whitespace-no-wrap">
+                                        <TrashIcon class="text-red-500 h-4 w-4" />
+                                    </button>
+
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, router, Link } from '@inertiajs/vue3';
+import Pagination from '@/Components/Pagination.vue'
+import { EyeIcon, PencilIcon, TrashIcon } from '@heroicons/vue/24/outline';
+
+const add_resource = () => {
+    router.get(route('resources.new'));
+}
+
+</script>
