571d7bd245266a62e18d68466fd46eb0943a07c0
luis franco paucar rodrigo <luis@MacBook-Pro-de-luis.local>
2023-12-23 20:02:38 -0500

    last merge the 23-12-23

--- resources/js/Pages/HumanResource/ManagementVacation/VacationInformation.vue
+++ resources/js/Pages/HumanResource/ManagementVacation/VacationInformation.vue
@@ -8,81 +8,101 @@
         <form @submit.prevent="submit">
             <div class="space-y-12">
                 <div class="border-b border-gray-900/10 pb-12">
-                    <h2 class="text-base font-semibold leading-7 text-gray-900">Informacion sobre las vacaciones del empleado</h2>
+                    <h2 class="text-base font-semibold leading-7 text-gray-900">Informacion sobre las vacaciones del
+                        empleado</h2>
                     <p class="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
 
                     <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                         <div class="sm:col-span-3">
                             <InputLabel for="employee" class="font-medium leading-6 text-gray-900">Empleado</InputLabel>
                             <div class="mt-2">
-                                <select v-model="form.employee_id" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                <select v-model="form.employee_id"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                     <option value="" disabled>Seleccione un empleado</option>
-                                    <option v-for="employee in employees" :key="employee.id" :value="employee.id">{{ employee.name + " " + employee.lastname}}</option>
-                                </select> 
-                            <InputError :message="form.errors.employee_id" />
-                        </div>
+                                    <option v-for="employee in employees" :key="employee.id" :value="employee.id">{{
+                                        employee.name + " " + employee.lastname }}</option>
+                                </select>
+                                <InputError :message="form.errors.employee_id" />
+                            </div>
                         </div>
                         <div class="sm:col-span-2">
-                            <InputLabel for="start_date" class="font-medium leading-6 text-gray-900">Fecha de Inicio</InputLabel>
+                            <InputLabel for="start_date" class="font-medium leading-6 text-gray-900">Fecha de Inicio
+                            </InputLabel>
                             <div class="mt-2">
-                                <TextInput type="Date" v-model="form.start_date" id="start_date" autocomplete="address-level1" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                                <InputError :message="form.errors.start_date"/>    
+                                <TextInput type="Date" v-model="form.start_date" id="start_date"
+                                    autocomplete="address-level1"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                <InputError :message="form.errors.start_date" />
                             </div>
                         </div>
 
                         <div class="sm:col-span-2">
-                            <InputLabel for="end_date" class="font-medium leading-6 text-gray-900">Fecha de Culminación</InputLabel>
+                            <InputLabel for="end_date" class="font-medium leading-6 text-gray-900">Fecha de Culminación
+                            </InputLabel>
                             <div class="mt-2">
-                                <TextInput type="Date" :value="form.end_date" v-model="form.end_date" id="end_date" autocomplete="address-level1" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                                <InputError :message="form.errors.end_date"/>    
+                                <TextInput type="Date" :value="form.end_date" v-model="form.end_date" id="end_date"
+                                    autocomplete="address-level1"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                <InputError :message="form.errors.end_date" />
                             </div>
                         </div>
 
                         <div class="sm:col-span-2">
                             <InputLabel for="reason" class="font-medium leading-6 text-gray-900">Razón</InputLabel>
-                                <div class="mt-2">
-                                <textarea v-model="form.reason" id="reason" autocomplete="postal-code" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                                <InputError :message="form.errors.reason"/>
+                            <div class="mt-2">
+                                <textarea v-model="form.reason" id="reason" autocomplete="postal-code"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                <InputError :message="form.errors.reason" />
                             </div>
                         </div>
-                        
+
                     </div>
                     <hr class="mt-5">
                     <h2 class="text-base font-semibold leading-7 text-gray-900 mt-5">Aceptar o rechazar solicitud</h2>
-                
+
                     <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 mt-5">
                         <div class="sm:col-span-2">
-                            <InputLabel for="start_date" class="font-medium leading-6 text-gray-900">Fecha de Inicio Aceptada</InputLabel>
+                            <InputLabel for="start_date" class="font-medium leading-6 text-gray-900">Fecha de Inicio
+                                Aceptada</InputLabel>
                             <div class="mt-2">
-                                <TextInput type="Date" v-model="form.start_date_accepted" id="start_date" autocomplete="address-level1" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />  
+                                <TextInput type="Date" v-model="form.start_date_accepted" id="start_date"
+                                    autocomplete="address-level1"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                             </div>
                         </div>
 
                         <div class="sm:col-span-2">
-                            <InputLabel for="end_date" class="font-medium leading-6 text-gray-900">Fecha de Culminación Aceptada</InputLabel>
+                            <InputLabel for="end_date" class="font-medium leading-6 text-gray-900">Fecha de Culminación
+                                Aceptada</InputLabel>
                             <div class="mt-2">
-                                <TextInput type="Date" v-model="form.end_date_accepted" id="end_date" autocomplete="address-level1" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" /> 
+                                <TextInput type="Date" v-model="form.end_date_accepted" id="end_date"
+                                    autocomplete="address-level1"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                             </div>
                         </div>
 
                         <div class="sm:col-span-3" v-if="route().params.vacation !== undefined">
                             <InputLabel for="status" class="font-medium leading-6 text-gray-900">Estado</InputLabel>
                             <div class="mt-2">
-                                <select v-model="form.status" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                <select v-model="form.status"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                     <option value="" disabled>Seleccione un estado</option>
                                     <option value="0">Pendiente</option>
                                     <option value="1">Activo</option>
-                                </select> 
-                            <InputError :message="form.errors.status" />
-                        </div>
+                                </select>
+                                <InputError :message="form.errors.status" />
+                            </div>
                         </div>
                     </div>
                 </div>
 
             </div>
             <div class="mt-6 flex items-center justify-end gap-x-6">
-                <button type="submit" :class="{ 'opacity-25': form.processing }" class="rounded-md bg-indigo-600 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Guardar</button>
-                <button type="button" v-if="route().params.vacation !== undefined" @click="destroy" :class="{ 'opacity-25': form.processing }" class="rounded-md bg-red-600 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Rechazar</button>
+                <button type="submit" :class="{ 'opacity-25': form.processing }"
+                    class="rounded-md bg-indigo-600 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Guardar</button>
+                <button type="button" v-if="route().params.vacation !== undefined" @click="destroy"
+                    :class="{ 'opacity-25': form.processing }"
+                    class="rounded-md bg-red-600 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Rechazar</button>
             </div>
         </form>
     </AuthenticatedLayout>
@@ -109,7 +129,7 @@ const form = useForm({
     end_date: '',
     start_date_accepted: '',
     end_date_accepted: '',
-    reason: '',   
+    reason: '',
     status: '0',
 });
 
@@ -160,8 +180,6 @@ const destroy = () => {
         }
     });
 };
-
-
 
 </script>
 
