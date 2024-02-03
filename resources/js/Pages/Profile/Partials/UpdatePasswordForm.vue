14cb42c9743b47fe07eef1579a5ac1e282b5d89f
luis franco paucar rodrigo <luis@MacBook-Pro-de-luis.local>
2024-02-03 11:49:57 -0500

    commit with spanish

--- resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
+++ resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
@@ -36,7 +36,7 @@ const updatePassword = () => {
 <template>
     <section>
         <header>
-            <h2 class="text-lg font-medium text-gray-900">Actualiza contraseña
+            <h2 class="text-lg font-medium text-gray-900">Actualizar contraseña
 </h2>
 
             <p class="mt-1 text-sm text-gray-600">
