0d5530de5685a1c6a8a9196ad35c861feb3d2c01
LuisPaucarRodrigo <luis.paucar.ro@gmail.com>
2024-02-13 21:06:53 -0500

    13-02-24 preproject and customervisit

---
+++ app/Models/Customervisit.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Customervisit extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'customer',
+        'phone',
+        'descritpion',
+        'address',
+        'date',
+        'observation',
+        'facade'
+    ];
+
+    public function preproject() {
+        return $this->HasOne(Preproject::class);
+    }
+}
