d1fb7188e22396f586c27785e097a66a2d894ee7
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2024-02-20 09:33:13 -0500

    accepCotization

---
+++ resources/js/Components/ConfirmAcceptModal.vue
@@ -0,0 +1,39 @@
+<template>
+    <Modal :show="confirmingaccept" :maxWidth="'sm'">
+        <div class="p-6">
+            <div class="text-center">
+                <div class="bg-green-200 rounded-full h-12 w-12 flex mx-auto items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                    </svg>
+                </div>
+                <h2 class="pt-5 text-lg font-medium text-gray-900">
+                    Aprobación de {{ itemType }} exitosa.
+                </h2>
+                <p class="mt-1 text-sm text-gray-600">
+                    ¡El/la {{ itemType }} se aprobó correctamente!
+                </p>
+            </div>
+        </div>
+    </Modal>
+</template>
+<script>
+import Modal from '@/Components/Modal.vue';
+export default {
+    components: {
+        Modal
+    },
+    props: {
+        confirmingaccept: {
+            type: Boolean,
+            required: true
+        },
+        itemType: {
+            type: String,
+            required: true
+        },
+    },
+};
+</script>
+  
--- resources/js/Pages/ProjectArea/ProjectManagement/CreateProject.vue
+++ resources/js/Pages/ProjectArea/ProjectManagement/CreateProject.vue
@@ -226,7 +226,6 @@ const { employees, start_date, numberOfProjects, project, preprojects } = define
     preprojects: Object
 })
 
-console.log(preprojects)
 
 const initialState = {
     preproject_id: '',
