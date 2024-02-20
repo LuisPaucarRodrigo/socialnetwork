91099c63c8b571243ea53cbb10f430f357705990
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2024-02-20 08:23:46 -0500

    budgetChanges

--- database/migrations/2024_01_04_164139_create_budget_updates_table.php
+++ database/migrations/2024_01_04_164139_create_budget_updates_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('budget_updates', function (Blueprint $table) {
             $table->id();
+            $table->double('difference', 10, 2);
             $table->double('new_budget', 10, 2);
             $table->foreignId('project_id')->constrained('projects')->onDelete('cascade');
             $table->text('reason');
