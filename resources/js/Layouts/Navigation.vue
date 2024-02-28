218a74382865cdd54ce80070064623bc18130a7c
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-02-28 01:07:38 -0500

    terminado vista view de orden de compra

--- resources/js/Layouts/Navigation.vue
+++ resources/js/Layouts/Navigation.vue
@@ -156,7 +156,7 @@
                 <a class="flex items-center mt-4 py-2 px-6 text-gray-100" href="#"
                     @click="showingShoppingArea = !showingShoppingArea">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6">
+                        :stroke="purchaseOrdersAlarms.length > 0 ? 'red' : 'currentColor'" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                     </svg>
@@ -169,46 +169,30 @@
                     <Link class="w-full" :href="route('purchasesrequest.index')">Solicitud de Cotizaciones</Link>
                 </MyTransition>
                 <MyTransition :transitiondemonstration="showingShoppingArea">
-                    <Link class="w-full" :href="route('purchaseorders.index')">Ordenes de compra</Link>
+                    <div class="relative">
+                        <button @click="showPurchaseOrdersAlarms = !showPurchaseOrdersAlarms">
+                            <span v-if="purchaseOrdersAlarms.length>0" class="absolute top-0 right-0 bg-red-500 text-white rounded-full h-6 w-6 flex items-center justify-center text-xs leading-4">{{ purchaseOrdersAlarms.length }}</span>
+                        </button>
+                        <Link class="w-full" :href="route('purchaseorders.index')">Ordenes de compra</Link>
+                    </div>
                 </MyTransition>
+                <!-- purchase order alarms -->
+                <template v-if="purchaseOrdersAlarms.length !== 0">
+                    <MyTransition v-for="item in purchaseOrdersAlarms" :key="item.id" class="ml-4" :transitiondemonstration="showPurchaseOrdersAlarms">
+                        <Link class="w-full flex items-center" :href="route('purchaseorders.details', {purchase_order_id: item.id})"
+                            >
+                        <svg :class="`w-4 h-4 mr-2 ${item.critical ? 'text-red-600' : 'text-yellow-400'} dark:text-red`" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M15.133 10.632v-1.8a5.407 5.407 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V1.1a1 1 0 0 0-2 0v2.364a.944.944 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C4.867 13.018 3 13.614 3 14.807 3 15.4 3 16 3.538 16h12.924C17 16 17 15.4 17 14.807c0-1.193-1.867-1.789-1.867-4.175Zm-13.267-.8a1 1 0 0 1-1-1 9.424 9.424 0 0 1 2.517-6.39A1.001 1.001 0 1 1 4.854 3.8a7.431 7.431 0 0 0-1.988 5.037 1 1 0 0 1-1 .995Zm16.268 0a1 1 0 0 1-1-1A7.431 7.431 0 0 0 15.146 3.8a1 1 0 0 1 1.471-1.354 9.425 9.425 0 0 1 2.517 6.391 1 1 0 0 1-1 .995ZM6.823 17a3.453 3.453 0 0 0 6.354 0H6.823Z" />
+                        </svg>
+                        <span>{{ item.purchase_quote.purchasing_requests.title }}</span>
+                        </Link>
+                    </MyTransition>
+                </template>
                 <MyTransition :transitiondemonstration="showingShoppingArea">
                     <Link class="w-full" :href="route('purchaseorders.history')">Registro de Compras</Link>
                 </MyTransition>
-
-
-                <template v-if="hasPermission('UserManager') && totalPurchaseOrdersAlarms !== 0">
-                    <a class="flex items-center ml-4 mt-4 py-2 px-6 text-gray-100" href="#" @click="showPurchaseOrdersAlarms = !showPurchaseOrdersAlarms">
-                        <div v-if="isCriticalPurchaseOrdersAlarms">
-                            <svg class="w-4 h-4 text-red-600 dark:text-red" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path
-                                    d="M15.133 10.632v-1.8a5.407 5.407 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V1.1a1 1 0 0 0-2 0v2.364a.944.944 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C4.867 13.018 3 13.614 3 14.807 3 15.4 3 16 3.538 16h12.924C17 16 17 15.4 17 14.807c0-1.193-1.867-1.789-1.867-4.175Zm-13.267-.8a1 1 0 0 1-1-1 9.424 9.424 0 0 1 2.517-6.39A1.001 1.001 0 1 1 4.854 3.8a7.431 7.431 0 0 0-1.988 5.037 1 1 0 0 1-1 .995Zm16.268 0a1 1 0 0 1-1-1A7.431 7.431 0 0 0 15.146 3.8a1 1 0 0 1 1.471-1.354 9.425 9.425 0 0 1 2.517 6.391 1 1 0 0 1-1 .995ZM6.823 17a3.453 3.453 0 0 0 6.354 0H6.823Z" />
-                            </svg>
-                        </div>
-                        <div v-else>
-                            <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 21">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6"
-                                    d="M8 3.464V1.1m0 2.365a5.338 5.338 0 0 1 5.133 5.368v1.8c0 2.386 1.867 2.982 1.867 4.175C15 15.4 15 16 14.462 16H1.538C1 16 1 15.4 1 14.807c0-1.193 1.867-1.789 1.867-4.175v-1.8A5.338 5.338 0 0 1 8 3.464ZM4.54 16a3.48 3.48 0 0 0 6.92 0H4.54Z" />
-                            </svg>
-                        </div>
-                        <span class=" ml-2.5">Por vencer: {{ totalPurchaseOrdersAlarms }}</span>
-                    </a>
-                    <div class="mb-4">
-                        <MyTransition v-for="item in purchaseOrdersAlarms" :key="item.id" class="ml-4" :transitiondemonstration="showPurchaseOrdersAlarms">
-                            <Link class="w-full flex items-center" :href="'#'"
-                                >
-                            <svg class="w-3 h-3 mr-2 text-red-600 dark:text-red" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path
-                                    d="M15.133 10.632v-1.8a5.407 5.407 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V1.1a1 1 0 0 0-2 0v2.364a.944.944 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C4.867 13.018 3 13.614 3 14.807 3 15.4 3 16 3.538 16h12.924C17 16 17 15.4 17 14.807c0-1.193-1.867-1.789-1.867-4.175Zm-13.267-.8a1 1 0 0 1-1-1 9.424 9.424 0 0 1 2.517-6.39A1.001 1.001 0 1 1 4.854 3.8a7.431 7.431 0 0 0-1.988 5.037 1 1 0 0 1-1 .995Zm16.268 0a1 1 0 0 1-1-1A7.431 7.431 0 0 0 15.146 3.8a1 1 0 0 1 1.471-1.354 9.425 9.425 0 0 1 2.517 6.391 1 1 0 0 1-1 .995ZM6.823 17a3.453 3.453 0 0 0 6.354 0H6.823Z" />
-                            </svg>
-                            <span>{{ item.purchase_quote.purchasing_requests.title }}</span>
-                            </Link>
-                        </MyTransition>
-                    </div>
-                </template>
-
             </template>
 
             <template v-if="hasPermission('UserManager')">
@@ -348,11 +332,7 @@ export default {
             cicsasubSectionsCount7: 0,
             cicsasubSectionsPorVencer: [],
             cicsasubSectionsPorVencer7: [],
-
-            totalPurchaseOrdersAlarms: 0,
             purchaseOrdersAlarms: [],
-            
-
             financePurchases: 0,
         };
     },
@@ -370,7 +350,7 @@ export default {
         let cicsashowingMembers7 = ref(false)
 
         let showPurchaseOrdersAlarms = ref(false)
-        let isCriticalPurchaseOrdersAlarms = ref(false)
+
         return {
             showingUsersAndRols,
             showingHumanResource,
@@ -382,7 +362,6 @@ export default {
             showingMembers7,
             cicsashowingMembers,
             cicsashowingMembers7,
-            isCriticalPurchaseOrdersAlarms,
             showPurchaseOrdersAlarms,
         }
     },
@@ -436,9 +415,9 @@ export default {
         async fetchFinanceAlarms () {
             try{
                 const response = await axios.get(route('purchaseorders.alarms'));
-                this.isCriticalPurchaseOrdersAlarms = response.data.purchaseOrders3d.length !== 0 ? true : false
-                this.purchaseOrdersAlarms = response.data.purchaseOrders7d.concat(response.data.purchaseOrders3d)
-                this.totalPurchaseOrdersAlarms = this.purchaseOrdersAlarms.length
+                this.purchaseOrdersAlarms = [...response.data.purchaseOrders3d.map(i=>({...i, 'critical':true})),
+                    ...response.data.purchaseOrders7d
+                ]
             } catch (error) {
                 console.error('Error al obtener alarmas de finanzas:', error);
             }
