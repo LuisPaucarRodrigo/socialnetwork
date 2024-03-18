c77744a29dcc69748b86fbc233bb04622106b1a5
LuisPaucarRodrigo <luis.paucar.ro@gmail.com>
2024-03-18 16:40:54 -0500

    calendar luis

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\VacationController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,19 +16,10 @@ use App\Http\Controllers\VacationController;
 
 
 Route::post('/login', [ApiController::class, 'login']);
-Route::get('/preproject', [ApiController::class, 'preproject']);
-Route::get('/preprojectespecific/{id}', [ApiController::class, 'preprojectespecific']);
-Route::post('/preprojectimage', [ApiController::class, 'preprojectimage']);
-Route::post('/logout', [ApiController::class, 'logout']);
 Route::middleware('auth:sanctum')->group(function () {
-    // Route::get('/preproject', [ApiController::class, 'preproject']);
-    // Route::get('/preprojectespecific/{id}', [ApiController::class, 'preprojectespecific']);
-    // Route::post('/preprojectimage', [ApiController::class, 'preprojectimage']);
-    // Route::post('/logout', [ApiController::class, 'logout']);
+    Route::get('/preproject', [ApiController::class, 'preproject']);
+    Route::get('/preprojectespecific/{id}', [ApiController::class, 'preprojectespecific']);
+    Route::post('/preprojectimage', [ApiController::class, 'preprojectimage']);
+    Route::post('/logout', [ApiController::class, 'logout']);
 });
 
-// Route::get('/vacation', [VacationController::class, 'index']);
-// Route::get('/vacation/{vacation}', [VacationController::class, 'show']);
-// Route::post('/vacation', [VacationController::class, 'store']);
-// Route::put('/vacation/{vacation}', [VacationController::class, 'update']);
-// Route::delete('/vacation/{vacation}', [VacationController::class, 'destroy']);
