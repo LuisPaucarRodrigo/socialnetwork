85982531bd59413bddefc7b8acc237dfffc327ea
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-03-19 08:35:03 -0500

    arreglos

--- resources/js/Pages/ProjectArea/Customers/Customers.vue
+++ resources/js/Pages/ProjectArea/Customers/Customers.vue
@@ -82,7 +82,7 @@
                     <div>
                         <InputLabel for="ruc" class="font-medium leading-6 text-gray-900 mt-3">RUC</InputLabel>
                         <div class="mt-2">
-                        <input type="number" v-model="form.ruc" id="ruc" max="11"
+                        <input v-model="form.ruc" id="ruc" maxlength="11" minlength="11" pattern="(\d)*" 
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                         <InputError :message="form.errors.ruc" />
                         </div>
