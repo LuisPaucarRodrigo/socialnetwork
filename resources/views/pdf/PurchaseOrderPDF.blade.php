749cae65b242cc9f71c826329c7e24cbce84c04e
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2024-03-28 00:43:18 -0500

    order

---
+++ resources/views/pdf/PurchaseOrderPDF.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>{{ $purchase_order->purchase_quote->purchasing_requests->title }}</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            margin: 0;
+            padding: 0;
+            margin-bottom: 0; /* Esto elimina el espacio entre tablas */
+        }
+        .td-custom {
+            font-size: 14px;
+            padding-left: 10px;
+        }
+
+        .td-custom2 {
+            border: 1.5px solid #000;
+            font-size: 14px;
+            padding-left: 10px;
+        }
+    </style>
+</head>
+<body>
+
+    <table style="width: 100%;">
+        <tr>
+            <td style="width: 80px; vertical-align: middle;"><img src="image/projectimage/logo_ccip.jpeg" width="150px" alt="" style=""></td>
+            <td style="padding-left: 50px">
+                <p style="margin-top: 30px; margin-left: 120; line-height: 0.5; font-weight: bold; font-size: 18px">Orden de Compra</p>
+                <p style="margin-left: 120; line-height: 0.5;">Fecha: {{ \Illuminate\Support\Carbon::createFromFormat('Y-m-d', $purchase_order->purchase_quote->quote_deadline)->format('d/m/Y') }}</p>
+                <p style="margin-left: 120; line-height: 0.5;">Número de Orden: {{ $purchase_order->code }}</p>
+            </td>
+        </tr>
+    </table>
+
+    <table style="width: 100%; margin-top: 30px">
+        <tr>
+            <td style="width: 300px;  padding-right: 50px">
+                <div style="padding-left: 5px;">
+                    <p style="font-size: 14px; font-weight: bold; line-height: 0.5;">Facturar a:</p>
+                    <p style="font-size: 14px; line-height: 0.5;">CONPROCO S.R.L.</p>
+                    <p style="font-size: 14px; line-height: 0.5;">RUC: 20559246272</p>
+                    <p style="font-size: 14px; line-height: 1;">CAL.PERAL NRO. 119 INT. 102 --- (FRENTE CONTRALORIA) AREQUIPA - AREQUIPA - AREQUIPA</p>
+                    <p style="margin-top: 30px; font-size: 14px; font-weight: bold; line-height: 0.5;">Enviar a:</p>
+                    <p style="font-size: 14px; line-height: 0.5;">CONPROCO S.R.L.</p>
+                    <p style="font-size: 14px; line-height: 1;">CAL.PERAL NRO. 119 INT. 102 --- (FRENTE CONTRALORIA) AREQUIPA - AREQUIPA - AREQUIPA</p>
+                    <p style="font-size: 14px; line-height: 0.5;">AREQUIPA</p>
+                    <p style="font-size: 14px; line-height: 0.5;">Perú</p>
+                    <p style="margin-top: 30px; font-size: 14px; line-height: 0.5; font-weight: bold">Tipo de Pago: </p>
+                    <p style="font-size: 14px; line-height: 0.5;">{{ $purchase_order->purchase_quote->payment_type }}</p> 
+                </div>
+                
+            </td>
+            <td style="display: flex; justify-content: flex-start; align-content: flex-start; width: 325px; margin-left: 10px">
+                <div style="border: 1px solid #000;">
+                    <p style="padding-top: 15px; margin-right: 60px; padding-left: 5px; line-height: 0.5;">A: </p>
+                    <p style="margin-right: 60px; padding-left: 5px; line-height: 0.5;">{{ $purchase_order->purchase_quote->provider->company_name }}</p>
+                    <p style="margin-right: 60px; padding-left: 5px; line-height: 0.5;">RUC: {{ $purchase_order->purchase_quote->provider->ruc }}</p>
+                    <p style="margin-right: 60px; padding-left: 5px; line-height: 0.5;">Dirección: {{ $purchase_order->purchase_quote->provider->address }}</p>
+                    <p style="margin-right: 60px; padding-left: 5px; line-height: 0.5;">Cel: {{ $purchase_order->purchase_quote->provider->phone1 }}</p>
+                    <p style="margin-right: 60px; padding-left: 5px; line-height: 0.5;">Contacto: {{ $purchase_order->purchase_quote->provider->contact_name }}</p>
+                    <p style="margin-right: 60px; padding-left: 5px; line-height: 0.5;">Email: {{ $purchase_order->purchase_quote->provider->email }}</p>
+
+                </div>
+            </td>
+        </tr>
+    </table>
+
+
+
+    <table style="margin-top: 30px">
+        <tr>
+            <td style="border: 1px solid #000; width: 250px;  padding-right: 50px">
+                <div style="padding-left: 5px; padding-top: 15px">
+                    <p style="font-size: 14px; font-weight: bold; line-height: 0.5;">Persona de contacto:</p>
+                    <p style="font-size: 14px; line-height: 0.5;">CONPROCO S.R.L.</p>
+                    <p style="font-size: 14px; line-height: 0.5;">RUC: 20559246272</p>
+                </div>
+            </td>
+        </tr>
+    </table>
+
+    <table style="margin-top: 10px">
+        <tr>
+            <td style="width: 450px;  padding-right: 50px">
+                <div style="padding-top: 15px">
+                    <p style="font-size: 14px; line-height: 1;">Enviar factura junto a documento xml.</p>
+                </div>
+            </td>
+        </tr>
+    </table>
+    
+    <table class="mt-3" style="">
+        <thead>
+          <tr>
+            <th class="td-custom2"  scope="col" colspan="6" style="text-align: center; font-weight: normal;">Productos</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="td-custom2" style="width: 55px; text-align: center; font-weight: normal;">Partida</td>
+            <td class="td-custom2" style="width: 270px; text-align: center; font-weight: normal;">Código y Descripción</td>
+            <td class="td-custom2" style="width: 55px; text-align: center; font-weight: normal;">Cantidad</td>
+            <td class="td-custom2" style="width: 63px; text-align: center; font-weight: normal;">Moneda</td>
+            <td class="td-custom2" style="width: 80px; text-align: center; font-weight: normal;">Valor unitario</td>
+            <td class="td-custom2" style="width: 90px; text-align: center; font-weight: normal;">Valor total</td>
+          </tr>
+          @php
+            $total = 0;
+          @endphp 
+          @foreach ($purchase_order->purchase_quote->purchase_quote_products as $index => $item)
+          <tr>
+
+            <td class="td-custom2" style="text-align: right">1.0{{ $index+1 }}</td>
+            <td class="td-custom2" >{{ $item->purchase_product->code . ' - ' . $item->purchase_product->name }}</td>
+            <td class="td-custom2" style="text-align: center">{{ $item->quantity }}</td>
+            <td class="td-custom2" style="text-align: center">{{ $item->purchase_quote->currency }}</td>
+            <td class="td-custom2" style="text-align: right">
+                @if ($item->purchase_quote->currency == 'dolar')
+                    $ {{ !$item->purchase_quote->igv ? number_format($item->unitary_amount/1.18 , 2) : number_format($item->unitary_amount, 2) }}
+                @elseif ($item->purchase_quote->currency == 'sol')
+                    S/. {{ !$item->purchase_quote->igv ? number_format($item->unitary_amount/1.18 , 2) : number_format($item->unitary_amount, 2) }}
+                @endif
+            </td>
+            <td class="td-custom2" style="text-align: right">
+                @if ($item->purchase_quote->currency == 'dolar')
+                    ${{ !$item->purchase_quote->igv ? number_format($item->quantity * $item->unitary_amount/1.18, 2) : number_format($item->quantity * $item->unitary_amount, 2) }}
+                @elseif ($item->purchase_quote->currency == 'sol')
+                    S/. {{ !$item->purchase_quote->igv ? number_format($item->quantity * $item->unitary_amount/1.18, 2) : number_format($item->quantity * $item->unitary_amount, 2) }}
+                @endif
+            </td>
+          </tr>
+          @endforeach
+          @if (!$purchase_order->purchase_quote->igv)
+          <tr>
+            <th class="td-custom2"  scope="col" colspan="6" style="text-align: right; font-weight: normal; font-weight: bold">Este valor no incluye IGV: {{ $purchase_order->purchase_quote->currency == 'sol' ? 'S/. ' : '$ '}} {{ number_format($purchase_order->purchase_quote->purchase_quote_products->sum(function($item){
+                return !$item->purchase_quote->igv ? number_format($item->quantity * $item->unitary_amount/1.18, 2) : number_format($item->quantity * $item->unitary_amount, 2);
+            }), 2) }} </th>
+          </tr>
+          @else
+          <tr>
+            <th class="td-custom2"  scope="col" colspan="6" style="text-align: right; font-weight: normal; font-weight: bold">Este valor incluye IGV: {{ $purchase_order->purchase_quote->currency == 'sol' ? 'S/. ' : '$ '}} {{ number_format($purchase_order->purchase_quote->purchase_quote_products->sum(function($item){
+                return !$item->purchase_quote->igv ? $item->quantity * $item->unitary_amount/1.18 : $item->quantity * $item->unitary_amount;
+            }), 2) }} </th>
+          </tr>
+          @endif
+        </tbody>
+      </table>
+
+</body>
+</html>
