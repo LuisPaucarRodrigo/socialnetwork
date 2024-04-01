34524544050e87efd0b798ca829cfa8afcc2505d
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2024-04-01 17:44:56 -0500

    bladeFix

--- resources/views/pdf/PurchasingRequestPDF.blade.php
+++ resources/views/pdf/PurchasingRequestPDF.blade.php
@@ -28,17 +28,15 @@
     <tbody>
       <tr>
         <td class="td-custom" colspan="1" style="height: 70px;"><img src="image/projectimage/logo_ccip.jpeg" width="80px" alt=""></td>
-        <td class="td-custom" colspan="3" style="text-align: center; font-weight: bold; font-size: 18px">{{ $purchasing_request->title }}</td>
-        <td class="td-custom" colspan="2" style="text-align: center">{{ $purchasing_request->code }}</td>
+        <td class="td-custom" colspan="2" style="text-align: center; font-weight: bold; font-size: 18px">{{ $purchasing_request->title }}</td>
+        <td class="td-custom" colspan="1" style="text-align: center">{{ $purchasing_request->code }}</td>
       </tr>
       <tr>
         <td class="td-custom" style="width: 82.5px">Elaborado por:</td>
         <td class="td-custom" style="width: 150px" style="text-align: center; font-weight: bold;">CCIP</td>
-        <td class="td-custom" style="width: 82.5px">Fecha</td>
-        <td class="td-custom" style="width: 150px" style="text-align: right">{{ \Illuminate\Support\Carbon::createFromFormat('Y-m-d', $purchasing_request->due_date)->format('d/m/Y') }}</td>
-        <td class="td-custom" style="width: 82.5px">Fecha</td>
-        <td class="td-custom" style="width: 150px" style="text-align: right">{{ \Illuminate\Support\Carbon::createFromFormat('Y-m-d', $purchasing_request->due_date)->format('d/m/Y') }}</td>
-    </tr>
+        <td class="td-custom" style="width: 130px">Fecha</td>
+        <td class="td-custom" style="width: 110px" style="text-align: right">{{ \Illuminate\Support\Carbon::createFromFormat('Y-m-d H:i:s', $purchasing_request->created_at)->format('d/m/Y') }}</td>
+      </tr>
     </tbody>
   </table>
 
@@ -67,60 +65,7 @@
     </tbody>
   </table>
 
-  <table class="mt-3">
-    <tbody>
-      <tr>
-        <td class="td-custom" colspan="2" style="text-align: center; background: #2e75b5; font-weight: normal;">Condiciones Comerciales</td>
-      </tr>
-      <tr>
-        <td class="td-custom" style="width: 140px; border-right: none">Lugar de entrega</td>
-        <td class="td-custom" style="width: 562.5px; border-left: none">: MDD</td>
-      </tr>
-      <tr>
-        <td class="td-custom" style="width: 140px; border-right: none">Forma de Pago</td>
-        <td class="td-custom" style="width: 562.5px; border-left: none">: Según acuerdo Comercial</td>
-      </tr>
-    </tbody>
-  </table>
-  <table class="mt-3">
-    <tbody>
-      <tr>
-        <td class="td-custom" colspan="2" style="text-align: center; background: #2e75b5; font-weight: normal;">Cuentas Bancarias</td>
-      </tr>
-      <tr>
-        <td class="td-custom" style="width: 200px">Banco de Crédito del Perú (Soles)</td>
-        <td class="td-custom" style="width: 500.5px">215-2622816-0-06</td>
-      </tr>
-      <tr>
-        <td class="td-custom" style="width: 200px">CCI</td>
-        <td class="td-custom" style="width: 500.5px">002-21500262281600620</td>
-      </tr>
-      <tr>
-        <td class="td-custom" style="width: 200px">Caja Municipal Arequipa (Soles)</td>
-        <td class="td-custom" style="width: 500.5px">001-44342902100001001</td>
-      </tr>
-      <tr>
-        <td class="td-custom" style="width: 200px">CCI</td>
-        <td class="td-custom" style="width: 500.5px">803-001-001443429001-72</td>
-      </tr>
-      <tr>
-        <td class="td-custom" style="width: 200px">Caja Municipal Arequipa (Dólares)</td>
-        <td class="td-custom" style="width: 500.5px">001-44342902110102001</td>
-      </tr>
-      <tr>
-        <td class="td-custom" class="td-custom" style="width: 200px">CCI</td>
-        <td class="td-custom" style="width: 500.5px">803-001-001443429002-70</td>
-      </tr>
-      <tr>
-        <td class="td-custom" colspan="2" style="text-align: center; background: #2e75b5; font-weight: normal;">Cuenta de detracciones</td>
-      </tr>
-      <tr>
-        <td class="td-custom" style="width: 200px">Banco de la Nación (Soles)</td>
-        <td class="td-custom" style="width: 500.5px">00-101-426491</td>
-      </tr>
 
-    </tbody>
-  </table>
 </body>
 
 
