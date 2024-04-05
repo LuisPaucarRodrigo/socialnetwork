c6749ef5e866c4c9628a1b853d5c7a0a3f65c990
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2024-04-05 15:01:00 -0500

    seeders

--- database/migrations/2024_01_22_201101_create_warehouses_table.php
+++ database/migrations/2024_01_22_201101_create_warehouses_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
     {
         Schema::create('warehouses', function (Blueprint $table) {
             $table->id();
-            $table->string('code');
             $table->string('name');
             $table->text('description');
-            $table->string('person_in_change');
+            $table->string('person_in_charge');
             $table->string('category');
             $table->timestamps();
         });
