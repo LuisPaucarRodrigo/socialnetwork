23d06b04fa72d9a73631631628b7bfc0d10ef7d0
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2024-04-08 14:57:24 -0500

    liquidations

--- resources/js/Pages/Inventory/WarehouseManagement/InventoryForm.vue
+++ resources/js/Pages/Inventory/WarehouseManagement/InventoryForm.vue
@@ -18,12 +18,14 @@
                             <option disabled value="">Selecciona un producto</option>
                             <option v-for="product in props.purchase_products" :value="product.id">{{ product.name }}</option>
                         </select>
+                        <InputError :message="form.errors.purchase_product_id" />
                     </div>
                     <div>
                         <InputLabel for="quantity" class="font-medium leading-6 text-gray-900 mt-2">Cantidad</InputLabel>
                         <div>
                         <input type="number" id="quantity" v-model="form.quantity"
                             class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2" />
+                        <InputError :message="form.errors.quantity" />
                         </div>
                     </div>
                     <div>
@@ -31,6 +33,7 @@
                         <div>
                         <input type="number" step="0.01" id="unitary_price" v-model="form.unitary_price"
                             class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2" />
+                        <InputError :message="form.errors.unitary_price" />
                         </div>
                     </div>
 
@@ -39,6 +42,7 @@
                         <div>
                         <input type="date" id="entry_date" v-model="form.entry_date"
                             class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2" />
+                        <InputError :message="form.errors.entry_date" />
                         </div>
                     </div>
 
@@ -47,6 +51,7 @@
                         <div>
                         <input type="text" id="referral_guide" v-model="form.referral_guide"
                             class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2" />
+                        <InputError :message="form.errors.referral_guide" />
                         </div>
                     </div>
 
@@ -63,16 +68,14 @@
 
               </div>
               <br>
-              <div class="mt-6 flex items-center justify-between gap-x-6">
-                <button @click="cancel" type="button"
-                  class="w-full rounded-md bg-red-600 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                  Cancelar
-                </button>
-                <button type="submit" :class="{ 'opacity-25': form.processing }"
-                  class="w-full rounded-md bg-indigo-600 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                  Guardar
-                </button>
+              <div class="mt-6 flex justify-end">
+                  <button type="submit"
+                          :class="{ 'opacity-25': form.processing }"
+                          class="rounded-md bg-indigo-600 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                      Guardar
+                  </button>
               </div>
+
             </div>
           </div>
         </form>
@@ -106,29 +109,24 @@
     purchase_product_id: null
   });
 
-  const cancel = () => {
-    router.visit(route('warehouses.products', {warehouse: props.warehouseId}))
-  };
-
   const submit = () => {
-
-form.post(route('warehouses.storeNormalProduct', { warehouse: props.warehouseId }), {
-  onSuccess: () => {
-    form.reset();
-    showModal.value = true
-    setTimeout(() => {
-      showModal.value = false;
-      router.visit(route('warehouses.products', { warehouse: props.warehouseId }))
-    }, 2000);
-  },
-  onError: () => {
-    form.reset();
-  },
-  onFinish: () => {
-    form.reset();
-  }
-});
-};
+    form.post(route('warehouses.storeNormalProduct', { warehouse: props.warehouseId }), {
+      onSuccess: () => {
+        form.reset();
+        showModal.value = true
+        setTimeout(() => {
+          showModal.value = false;
+          router.visit(route('warehouses.products', { warehouse: props.warehouseId }))
+        }, 2000);
+      },
+      onError: () => {
+        form.reset();
+      },
+      onFinish: () => {
+        form.reset();
+      }
+    });
+  };
  
   </script>
   
