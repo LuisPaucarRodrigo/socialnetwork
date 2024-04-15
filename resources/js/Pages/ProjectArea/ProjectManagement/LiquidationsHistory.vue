ee78c3856396a28766c11676892ab463bd20d719
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2024-04-15 15:34:49 -0500

    fixLiquidation

---
+++ resources/js/Pages/ProjectArea/ProjectManagement/LiquidationsHistory.vue
@@ -0,0 +1,81 @@
+<template>
+    <Head title="Proyectos" />
+    <AuthenticatedLayout :redirectRoute="{route: 'projectmanagement.liquidate', params: { project_id: project_id}}">
+        <template #header>
+            Historial de Liquidaciones
+        </template>
+
+        <div class="inline-block min-w-full overflow-hidden rounded-lg shadow">
+            <div class="talwing mt-4">
+                <table class="w-full whitespace-no-wrap">
+                    <thead>
+                        <tr
+                            class="border-b bg-gray-50 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">
+                            <th
+                                class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                                Proucto
+                            </th>
+                            <th
+                                class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                                Cantidad Liquidada
+                            </th>
+                            <th
+                                class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                                Cantidad Devuelta
+                            </th>
+                            <th
+                                class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                                Devuelto a:
+                            </th>
+                            <th
+                                class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600">
+                                Observaciones
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="item in liquidations.data" :key="item.id" :class="[
+                            'text-gray-700',
+                            {
+                                'border-l-4': true,
+                            }
+                        ]">
+                            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                                <p class="text-gray-900 whitespace-no-wrap">{{ item.project_entry.special_inventory ? item.project_entry.special_inventory.purchase_product.name : item.project_entry.entry.inventory.purchase_product.name }}</p>
+                            </td>
+                            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                                <p class="text-gray-900 whitespace-no-wrap">{{ item.liquidated_quantity }}</p>
+                            </td>
+                            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                                <p class="text-gray-900 whitespace-no-wrap">{{ item.refund_quantity }}</p>
+                            </td>
+                            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                                <p class="text-gray-900 whitespace-no-wrap">{{ item.type }}</p>
+                            </td>
+                            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                                <p class="text-gray-900 whitespace-no-wrap">{{ item.observations }}</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="flex flex-col items-center border-t bg-white px-5 py-5 xs:flex-row xs:justify-between">
+            <pagination :links="liquidations.links" />
+        </div>
+
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, router, Link } from '@inertiajs/vue3';
+import Pagination from '@/Components/Pagination.vue';
+import { formattedDate } from '@/utils/utils';
+
+const { liquidations, project_id } = defineProps({
+    liquidations: Object,
+    project_id: String
+})
+
+</script>
