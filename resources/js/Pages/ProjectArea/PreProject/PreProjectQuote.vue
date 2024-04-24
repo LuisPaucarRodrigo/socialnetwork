66d350cf4740492d42d871a1ae6d5e67f677a2d2
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-04-24 16:12:00 -0500

    reformating reserved and used quantity

--- resources/js/Pages/ProjectArea/PreProject/PreProjectQuote.vue
+++ resources/js/Pages/ProjectArea/PreProject/PreProjectQuote.vue
@@ -136,7 +136,7 @@
                                     <h2 class="text-base font-bold leading-6 text-gray-900 ">Productos
                                     </h2>
 
-                                    <button v-if="auth.user.role_id === 1 || preproject.quote === null"
+                                    <!-- <button v-if="auth.user.role_id === 1 || preproject.quote === null"
                                         @click="openProductModal" type="button">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                             stroke-width="1.5" stroke="currentColor"
@@ -144,7 +144,7 @@
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="M7.5 7.5h-.75A2.25 2.25 0 0 0 4.5 9.75v7.5a2.25 2.25 0 0 0 2.25 2.25h7.5a2.25 2.25 0 0 0 2.25-2.25v-7.5a2.25 2.25 0 0 0-2.25-2.25h-.75m-6 3.75 3 3m0 0 3-3m-3 3V1.5m6 9h.75a2.25 2.25 0 0 1 2.25 2.25v7.5a2.25 2.25 0 0 1-2.25 2.25h-7.5a2.25 2.25 0 0 1-2.25-2.25v-.75" />
                                         </svg>
-                                    </button>
+                                    </button> -->
                                 </div>
                                 <p class="text-sm mt-6">
                                     Productos de las solicitudes de compra aceptadas
