fe6e8923deeb1981ebbe68c2dc466b36cf01e6df
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-05-01 15:53:43 -0500

    products state in purchasing request

--- app/Models/Purchasing_requests_product.php
+++ app/Models/Purchasing_requests_product.php
@@ -17,7 +17,10 @@ class Purchasing_requests_product extends Model
     ];
 
     protected $appends = [
-        'actual_quotes_quantity'
+        'actual_quotes_quantity',
+        'pending_quotes_quantity',
+        'accepted_quotes_quantity',
+        'completed_state',
     ];
 
     public function purchase_product()
@@ -30,12 +33,18 @@ class Purchasing_requests_product extends Model
     }
 
     public function getActualQuotesQuantityAttribute () {
+        return $this->getAcceptedQuotesQuantityAttribute()
+                + $this->getPendingQuotesQuantityAttribute();
+    }
+
+
+    public function getPendingQuotesQuantityAttribute () {
         $pr = $this->purchasing_request()
-            ->with('purchase_quotes.purchase_quote_products')
-            ->first();
+        ->with('purchase_quotes.purchase_quote_products')
+        ->first();
         $pqProductsTotal = 0;
         foreach ($pr->purchase_quotes as $quote){
-            if (($quote->state === null || $quote->state === 1)
+            if (($quote->state === null)
                 && ($quote->preproject_state === null || $quote->preproject_state === 1) ){
                 $key = array_search(
                     $this->purchase_product->id, 
@@ -45,9 +54,35 @@ class Purchasing_requests_product extends Model
                 $pqProductsTotal += $product->quantity;
             }
         }
-
-
         return $pqProductsTotal;
+    }
+
+
+    public function getAcceptedQuotesQuantityAttribute () {
+        $pr = $this->purchasing_request()
+        ->with('purchase_quotes.purchase_quote_products')
+        ->first();
+        $pqProductsTotal = 0;
+        foreach ($pr->purchase_quotes as $quote){
+            if (($quote->state === 1)
+                && ($quote->preproject_state === null || $quote->preproject_state === 1) ){
+                $key = array_search(
+                    $this->purchase_product->id, 
+                    array_column($quote->purchase_quote_products->toArray(), 'purchase_product_id')
+                );
+                $product = $quote->purchase_quote_products[$key];
+                $pqProductsTotal += $product->quantity;
+            }
+        }
+        return $pqProductsTotal;
+    }
+
+    
+    public function getCompletedStateAttribute() {
+        if( $this->getAcceptedQuotesQuantityAttribute() > $this->quantity) {
+            throw new \Exception('Error en productos de cotizaciones cantidades inválidas');
+        }
+        return $this->getAcceptedQuotesQuantityAttribute() === $this->quantity;
     }
 
 }
