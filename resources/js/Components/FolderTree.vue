0963cd2a47086acee0d6e92eea9d6cf651fd7ade
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-05-10 12:35:30 -0500

    test to show public archives

---
+++ resources/js/Components/FolderTree.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul>
+      <li v-for="(item, itemName, i) in folderStructure" :key="i">
+        <span :style="{ marginLeft: (level * 20) + 'px' }">{{ typeof item === 'object' ? itemName + ' (' + item.path + ')' : item }}</span>
+        <template v-if="typeof item === 'object'">
+          <folder-tree :folder-structure="item" :level="level + 1"></folder-tree>
+        </template>
+      </li>
+    </ul>
+  </template>
+  
+  <script>
+  export default {
+    name: 'FolderTree',
+    props: {
+      folderStructure: Object,
+      level: {
+        type: Number,
+        default: 0
+      }
+    }
+  }
+  </script>
+  
