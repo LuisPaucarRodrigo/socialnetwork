dbeb71aa880ab64c023e5c2e74180ef304b1a8b5
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-05-16 10:03:10 -0500

    dm template prototype

---
+++ app/Http/Controllers/DocumentManagement/FolderController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\DocumentManagement;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FolderController extends Controller
+{
+    public function folder_index() {
+        
+    }
+}
