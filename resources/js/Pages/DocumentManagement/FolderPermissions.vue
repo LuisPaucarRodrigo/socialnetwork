f8b503efcea3aea43efb4f8555528e3e5211dc7b
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-05-18 00:24:00 -0500

    template optimizing modals of permissions

--- resources/js/Pages/DocumentManagement/FolderPermissions.vue
+++ resources/js/Pages/DocumentManagement/FolderPermissions.vue
@@ -38,7 +38,7 @@
                             <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
                                 <div class="flex justify-center">
                                     <input type="checkbox" :checked="item.see_download"
-                                        @input="see_download_handler($event.target.checked, item.id)"
+                                        @input="openPermissionhandleModal($event.target.checked, item.id, modalOptions.see_download)"
                                         class="block border-0 py-1.5 text-gray-900 shadow-sm ring-1 h-5 w-5 ring-inset placeholder:text-gray-400 sm:text-sm sm:leading-6" />
                                 </div>
                             </td>
@@ -54,6 +54,36 @@
                     </tbody>
                 </table>
             </div>
+
+            <Modal :show="showPermissionHandler" @close="closePermissionHandlerModal">
+            <div class="p-6">
+                <h2 class="text-lg font-medium text-gray-900">
+                    
+                </h2>
+
+                <p class="mt-1 text-sm text-gray-600">
+                    Una vez que se elimine la cuenta, todos sus recursos y datos se eliminarán permanentemente. Por favor
+                    ingrese su contraseña para confirmar que desea eliminar permanentemente la cuenta.
+                </p>
+
+                <div class="mt-6">
+                    <InputLabel for="password" value="Password" class="sr-only" />
+                    <TextInput id="password" ref="passwordInput" v-model="form.password" type="password"
+                        class="mt-1 block w-3/4" placeholder="Password" @keyup.enter="deleteUser" />
+
+                    <InputError :message="form.errors.password" class="mt-2" />
+                </div>
+
+                <div class="mt-6 flex justify-end">
+                    <SecondaryButton @click="closeModal"> Cancel </SecondaryButton>
+
+                    <DangerButton class="ml-3" :class="{ 'opacity-25': form.processing }" :disabled="form.processing"
+                        @click="deleteUser">
+                        Delete Account
+                    </DangerButton>
+                </div>
+            </div>
+        </Modal>
 
         </div>
     </AuthenticatedLayout>
@@ -85,6 +115,44 @@ function see_download_handler (state, id ) {
             console.log('siuuuuuuuuuu')
         }
     })
+}
+
+
+const showPermissionHandler = ref(false)
+
+const modalOptions = {
+    see_download: 'see_download',
+    create: 'create'
+}
+
+const initialModalItem = {
+    title:'',
+    message: '',
+    option: '',
+    postItem: {
+        folder_area_id:'',
+        state:'',
+        down_recursive:false
+    }
+}
+
+const modalItem = ref(JSON.parse(JSON.stringify(initialModalItem))) 
+
+
+function openPermissionhandleModal (state, id, option) {
+    if(state && option === modalOptions.see_download){
+        modalItem.value = {
+            title: 'Activar permiso de ver y descargar',
+            message: 'Al realizar esta acción activará el permiso de ver y descargar de la actual carpeta y de las carpetas superiores a estas',
+            option: option,
+            postItem:{
+                folder_area_id:id,
+                state: state,
+                down_recursive:false
+            }
+        }
+        showPermissionHandler.value = true
+    }
 }
 
 
