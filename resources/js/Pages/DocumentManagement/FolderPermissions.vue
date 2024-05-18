f1bb377b2037450c47717d996d09eb64110a9659
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-05-18 12:44:50 -0500

    delete permission template

--- resources/js/Pages/DocumentManagement/FolderPermissions.vue
+++ resources/js/Pages/DocumentManagement/FolderPermissions.vue
@@ -58,6 +58,7 @@
                             <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
                                 <button>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        @click="openDeleteFAModal(item.id)"
                                         stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-red-500">
                                         <path stroke-linecap="round" stroke-linejoin="round"
                                             d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -113,7 +114,7 @@
                         Agregar Nueva Área con Permisos
                     </h2>
                     <InputLabel>
-                        Se Agregará el siguiente Área con los permisos de ver, descargar y ver. 
+                        Se Agregará el siguiente Área con los permisos de ver, descargar y ver.
                     </InputLabel>
                     <InputLabel class="mb-5">
                         Las áreas disponibles para selección corresponden a las de la carpeta superior inmediata.
@@ -125,22 +126,40 @@
                                 <option value="" disabled>
                                     Selecciona un area
                                 </option>
-                                <option v-for="item in makeAreasArray(permissions, upper_folder_areas)" :key="item.id" :value="item.id">
+                                <option v-for="item in makeAreasArray(permissions, upper_folder_areas)" :key="item.id"
+                                    :value="item.id">
                                     {{ `${item.name}` }}
                                 </option>
                             </select>
                             <InputError :message="addFolderAreaform.errors.employees" />
                         </div>
                         <div class="mt-6 flex justify-end">
-                        <SecondaryButton @click="closeAddFAModal"> Cancelar </SecondaryButton>
+                            <SecondaryButton @click="closeAddFAModal"> Cancelar </SecondaryButton>
 
-                        <PrimaryButton type="submit" class="ml-3" customClass="tracking-widest" >
-                            Aceptar
-                        </PrimaryButton>
-                    </div>
+                            <PrimaryButton type="submit" class="ml-3" customClass="tracking-widest">
+                                Aceptar
+                            </PrimaryButton>
+                        </div>
                     </form>
+                </div>
+            </Modal>
 
-                    
+            <Modal :show="showDeleteFA" @close="closeDeleteFAModal">
+                <div class="p-6">
+                    <h2 class="text-lg font-medium text-gray-900 mb-2">
+                        Eliminar Permisos de Área
+                    </h2>
+                    <InputLabel class="mb-5">
+                        Al eliminar el permiso de esta área se eliminaran los permisos de todas sus subcarpeatas.
+                    </InputLabel>
+
+                    <div class="mt-6 flex justify-end">
+                        <SecondaryButton @click="closeDeleteFAModal"> Cancelar </SecondaryButton>
+
+                        <DangerButton type="button" class="ml-3">
+                            Eliminar
+                        </DangerButton>
+                    </div>
                 </div>
             </Modal>
         </div>
@@ -162,15 +181,15 @@ import PrimaryButton from '@/Components/PrimaryButton.vue';
 
 
 
-const { permissions, folder, upper_folder_areas} = defineProps({
+const { permissions, folder, upper_folder_areas } = defineProps({
     permissions: Object,
     folder: Object,
     upper_folder_areas: Object
 })
 
-const makeAreasArray = (perArray,ufArray) => {
-   let permissionsAreasIds = perArray.map(item=>item.area_id);
-   return ufArray.filter(item=>!permissionsAreasIds.includes(item.id)) 
+const makeAreasArray = (perArray, ufArray) => {
+    let permissionsAreasIds = perArray.map(item => item.area_id);
+    return ufArray.filter(item => !permissionsAreasIds.includes(item.id))
 }
 
 function see_download_handler(state, id) {
@@ -308,7 +327,7 @@ function closeAddFAModal() {
 }
 
 
-function addFolderArea () {
+function addFolderArea() {
     console.log('llegue')
     addFolderAreaform.post(route('documment.management.folders.permission.add'), {
         onSuccess: () => {
@@ -317,6 +336,21 @@ function addFolderArea () {
             console.log(e)
         }
     })
+}
+
+
+//delete folder area
+const showDeleteFA = ref(false)
+const folderAreaIdToDelete = ref(null)
+
+function openDeleteFAModal (id) {
+    showDeleteFA.value = true
+    folderAreaIdToDelete.value = id
+}
+
+function closeDeleteFAModal () {
+    showDeleteFA.value = false
+    folderAreaIdToDelete.value = null
 }
 
 </script>
