b6896d7e9d98d01a4a56b3f2df1ac955678e6fb8
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-05-22 18:42:14 -0500

    models and relation created sn sots

---
+++ app/Models/SNSotLiquidation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SNSotLiquidation extends Model
+{
+    use HasFactory;
+    protected $table = 'sn_sot_liquidation';
+    protected $fillable = [
+        'sot_id',
+        'up_minutes',
+        'liquidation',
+        'down_warehouse',
+        'liquidation_date',
+        'sot_status',
+    ];
+
+    public function sot() {
+        return $this->belongsTo(SNSot::class, 'sot_id');
+    }
+}
