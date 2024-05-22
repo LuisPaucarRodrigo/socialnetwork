72af7041f731c017644eb2ac303d01a49532d3ef
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-05-22 18:31:10 -0500

    all migrations new module sn sots

---
+++ database/migrations/2024_05_22_180345_create_sn_sot_payment_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sn_sot_payment', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sot_id')->constrained('sn_sots')->onDelete('cascade');
+            $table->string('sot_bill')->nullable();
+            $table->string('sot_bill_date')->nullable();
+            $table->string('bill')->nullable();
+            $table->string('bill_date')->nullable();
+            $table->string('charge')->nullable();
+            $table->string('charge_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sn_sot_payment');
+    }
+};
