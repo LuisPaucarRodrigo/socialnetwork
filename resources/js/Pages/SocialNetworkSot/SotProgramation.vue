aa7adaab65ee661f58ef068a6e2d2cf545baadb7
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-05-23 17:17:02 -0500

    programation store sot ns completed

--- resources/js/Pages/SocialNetworkSot/SotProgramation.vue
+++ resources/js/Pages/SocialNetworkSot/SotProgramation.vue
@@ -43,28 +43,36 @@
                     <tbody>
                         <tr v-for="item in sots.data" :key="item.id" class="text-gray-700">
                             <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
-                                <p class="text-gray-900 text-center">{{ item.name }}</p>
+                                <p class="text-gray-900 text-center">
+                                    {{ item.name }}
+                                </p>
                             </td>
                             <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
-                                <p class="text-gray-900 text-left">{{ item.platform }}</p>
+                                <p class="text-gray-900 text-center">
+                                    {{ item.description }}
+                                </p>
                             </td>
                             <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
-                                <p class="text-gray-900 text-center">{{ item.email }}</p>
+                                <p class="text-gray-900 text-center">
+                                    {{ formattedDate(item.assigned_date) }}
+                                </p>
                             </td>
                             <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
-                                <p class="text-gray-900 text-center">{{ item.dni }}</p>
+                                <p class="text-gray-900 text-center">
+                                    {{ item?.user_assignee?.name }}
+                                </p>
                             </td>
                             <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
                                 <div class="flex space-x-3 justify-center">
-                                    
-                                    
-                                    <Link class="text-blue-900">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-amber-400">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                                    </svg>
-                                    </Link>
+
+
+                                    <button class="text-blue-900">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-amber-400">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                        </svg>
+                                    </button>
                                 </div>
                             </td>
                         </tr>
@@ -76,7 +84,7 @@
                 <pagination :links="sots.links" />
             </div>
         </div>
-        
+
         <Modal :show="showAddSotModal" @close="closeAddSotModal">
             <div class="p-6">
                 <h2 class="text-lg font-medium text-gray-800 border-b-2 border-gray-100">
@@ -88,28 +96,26 @@
                         <div class="">
                             <InputLabel>SOT</InputLabel>
                             <div class="mt-2">
-                                <input type="number" v-model="formSot.name"
-                                    autocomplete="off"
+                                <input type="number" v-model="formSot.name" autocomplete="off"
                                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                                 <InputError :message="formSot.errors.name" />
                             </div>
                         </div>
-                        
+
                         <div class="">
                             <InputLabel>Fecha de Asignación</InputLabel>
                             <div class="mt-2">
-                                <input type="date" v-model="formSot.assigned_date"
-                                    autocomplete="off"
+                                <input type="date" v-model="formSot.assigned_date" autocomplete="off"
                                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                                 <InputError :message="formSot.errors.assigned_date" />
                             </div>
-                        </div> 
+                        </div>
                         <div>
                             <InputLabel>
                                 Persona Designada
                             </InputLabel>
                             <div class="mt-2">
-                                <select v-model="formSot.user_assignee_id" 
+                                <select v-model="formSot.user_assignee_id"
                                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                     <option value="" disabled>Seleccionar</option>
                                     <option v-for="item in snop_users" :key="item.id" :value="item.id">
@@ -123,9 +129,8 @@
                         <div class="">
                             <InputLabel>Descripción</InputLabel>
                             <div class="mt-2">
-                                <textarea v-model="formSot.description"
-                                    autocomplete="off"
-                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" ></textarea>
+                                <textarea v-model="formSot.description" autocomplete="off"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                                 <InputError :message="formSot.errors.description" />
                             </div>
                         </div>
@@ -134,8 +139,8 @@
                     <div class="mt-6 flex justify-end">
                         <SecondaryButton type="button" @click="closeAddSotModal"> Cancelar </SecondaryButton>
 
-                        <PrimaryButton class="ml-3 tracking-widest uppercase text-xs" :class="{ 'opacity-25': formSot.processing }"
-                            :disabled="formSot.processing" type="submit">
+                        <PrimaryButton class="ml-3 tracking-widest uppercase text-xs"
+                            :class="{ 'opacity-25': formSot.processing }" :disabled="formSot.processing" type="submit">
                             Guardar
                         </PrimaryButton>
                     </div>
@@ -162,6 +167,7 @@ import { ref } from 'vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import SelectSNSotComponent from '@/Components/SelectSNSotComponent.vue';
 import SuccessOperationModal from '@/Components/SuccessOperationModal.vue';
+import {formattedDate} from '@/utils/utils.js';
 
 const { sots, snop_users, auth } = defineProps({
     sots: Object,
@@ -178,17 +184,17 @@ const initialState = {
     assigned_date: ''
 }
 const formSot = useForm(
-    {...initialState}
+    { ...initialState }
 );
 
 
 //Add SOT
 const showAddSotModal = ref(false);
 const confirmSot = ref(false);
-function openAddSotModal () {
+function openAddSotModal() {
     showAddSotModal.value = true
 }
-function closeAddSotModal () {
+function closeAddSotModal() {
     showAddSotModal.value = false
     formSot.reset()
 }
@@ -199,13 +205,13 @@ function submit() {
 
 }
 
-function submitStore () {
+function submitStore() {
     let url = route('socialnetwork.sot.programation.store')
     formSot.post(url, {
         onSuccess: () => {
             closeAddSotModal()
             confirmSot.value = true
-            setTimeout(()=>{
+            setTimeout(() => {
                 confirmSot.value = false
             }, 1500)
         }
