ccb93308e21345b61fa2ba256ea853f118ce000e
LuisPaucarRodrigo <luis.paucar.ro@gmail.com>
2024-05-28 17:44:36 -0500

    delete depen

--- routes/finance_route.php
+++ routes/finance_route.php
@@ -3,23 +3,31 @@
 use App\Http\Controllers\Finance\BudgetUpdateController;
 use App\Http\Controllers\Finance\DepositController;
 use App\Http\Controllers\Finance\ExpenseManagementController;
+use App\Http\Controllers\Finance\PaymentController;
 use Illuminate\Support\Facades\Route;
 
-    Route::any('/finance/expencemanagement/{boolean?}', [ExpenseManagementController::class, 'index'])->name('managementexpense.index');
-    Route::get('/finance/expencemanagement/details/{purchase_quote}', [ExpenseManagementController::class, 'details'])->name('managementexpense.details');
+Route::any('/finance/expencemanagement/{boolean?}', [ExpenseManagementController::class, 'index'])->name('managementexpense.index');
+Route::get('/finance/expencemanagement/details/{purchase_quote}', [ExpenseManagementController::class, 'details'])->name('managementexpense.details');
 
-    Route::put('/finance/expencemanagement/reviewed/{id}', [ExpenseManagementController::class, 'reviewed'])->name('managementexpense.reviewed');
-    Route::get('/finance/expencemanagement/generate_payment/{id}', [ExpenseManagementController::class, 'generate_payment'])->name('managementexpense.payment');
-    Route::get('/finance/purchase_quotes/doTask', [ExpenseManagementController::class, 'approve_quote_alarm'])->name('approve_quote.alarm');
-    //Budget
-    Route::get('/budgetUpdates/{project}', [BudgetUpdateController::class, 'index'])->name('budgetupdates.index');
-    Route::get('/selectProject', [BudgetUpdateController::class, 'selectProject'])->name('selectproject.index');
-    Route::get('/budgetUpdates/{project}/{budgetupdate}', [BudgetUpdateController::class, 'show'])->name('budgetupdates.show');
-    Route::get('/initialBudget/{project}', [BudgetUpdateController::class, 'initial'])->name('initialbudget.index');
-    Route::post('/initialBudget/{project}/createUpdate', [BudgetUpdateController::class, 'create'])->name('budgetupdates.create');
-    Route::put('/initialBudget/{project}/define', [BudgetUpdateController::class, 'define_initial_budget'])->name('initialbudget.define');
+Route::put('/finance/expencemanagement/reviewed/{id}', [ExpenseManagementController::class, 'reviewed'])->name('managementexpense.reviewed');
+Route::get('/finance/expencemanagement/generate_payment/{id}', [ExpenseManagementController::class, 'generate_payment'])->name('managementexpense.payment');
+Route::get('/finance/purchase_quotes/doTask', [ExpenseManagementController::class, 'approve_quote_alarm'])->name('approve_quote.alarm');
+//Budget
+Route::get('/budgetUpdates/{project}', [BudgetUpdateController::class, 'index'])->name('budgetupdates.index');
+Route::get('/selectProject', [BudgetUpdateController::class, 'selectProject'])->name('selectproject.index');
+Route::get('/budgetUpdates/{project}/{budgetupdate}', [BudgetUpdateController::class, 'show'])->name('budgetupdates.show');
+Route::get('/initialBudget/{project}', [BudgetUpdateController::class, 'initial'])->name('initialbudget.index');
+Route::post('/initialBudget/{project}/createUpdate', [BudgetUpdateController::class, 'create'])->name('budgetupdates.create');
+Route::put('/initialBudget/{project}/define', [BudgetUpdateController::class, 'define_initial_budget'])->name('initialbudget.define');
 
-    Route::get('/finance/desposits', [DepositController::class, 'deposits_index'])->name('deposits.index');
-    Route::post('/finance/desposits/{deposit_id?}', [DepositController::class, 'deposits_store'])->name('deposits.store');
-    //
-    Route::post('/finance/desposits/generateSummary/post', [DepositController::class, 'generateSummary'])->name('deposits.generateSummary');
+Route::get('/finance/desposits', [DepositController::class, 'deposits_index'])->name('deposits.index');
+Route::post('/finance/desposits/{deposit_id?}', [DepositController::class, 'deposits_store'])->name('deposits.store');
+//
+Route::post('/finance/desposits/generateSummary/post', [DepositController::class, 'generateSummary'])->name('deposits.generateSummary');
+
+//PAYMENT
+Route::get('/finance/payment', [PaymentController::class, 'index'])->name('payment.index');
+Route::get('/finance/payment/search/{request}', [PaymentController::class, 'search'])->name('payment.search');
+Route::post('/finance/payment/pay', [PaymentController::class, 'payment_pay'])->name('payment.pay');
+
+Route::get('/finance/alarm/payment', [PaymentController::class, 'alarm_payments'])->name('payment.alarm');
