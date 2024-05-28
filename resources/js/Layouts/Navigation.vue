26ddea8ead2425249a65d50bacb192305569cfef
LuisPaucarRodrigo <luis.paucar.ro@gmail.com>
2024-05-28 17:44:36 -0500

    delete depen

--- resources/js/Layouts/Navigation.vue
+++ resources/js/Layouts/Navigation.vue
@@ -311,43 +311,7 @@ Usuarios
                         </Link>
                     </MyTransition>
                 </template>
-                <MyTransition :transitiondemonstration="showingShoppingArea">
-                    <div class="relative">
-                        <button @click="tooglePayment"><span v-if="paymentAlarms3.length + paymentAlarms7.length > 0"
-                                class="absolute top-0 right-0 bg-red-500 text-white rounded-full h-6 w-6 flex items-center justify-center text-xs leading-4">
-                                {{ paymentAlarms3.length + paymentAlarms7.length }}</span>
-                        </button>
-                        <Link class="w-full" :href="route('payment.index')">Pagos OC</Link>
-                    </div>
-                </MyTransition>
-                <template v-if="paymentAlarms3.length !== 0">
-                    <MyTransition v-for="item in paymentAlarms3" :key="item.id" class="ml-4"
-                        :transitiondemonstration="paymentPorVencer">
-                        <Link class="w-full flex items-center" :href="route('payment.index')">
-                        <div class="flex items-center"> <!-- Contenedor del ícono y el título -->
-                            <svg class="w-4 h-4 mr-2 text-red-600 dark:text-red" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path
-                                    d="M15.133 10.632v-1.8a5.407 5.407 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V1.1a1 1 0 0 0-2 0v2.364a.944.944 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C4.867 13.018 3 13.614 3 14.807 3 15.4 3 16 3.538 16h12.924C17 16 17 15.4 17 14.807c0-1.193-1.867-1.789-1.867-4.175Zm-13.267-.8a1 1 0 0 1-1-1 9.424 9.424 0 0 1 2.517-6.39A1.001 1.001 0 1 1 4.854 3.8a7.431 7.431 0 0 0-1.988 5.037 1 1 0 0 1-1 .995Zm16.268 0a1 1 0 0 1-1-1A7.431 7.431 0 0 0 15.146 3.8a1 1 0 0 1 1.471-1.354 9.425 9.425 0 0 1 2.517 6.391 1 1 0 0 1-1 .995ZM6.823 17a3.453 3.453 0 0 0 6.354 0H6.823Z" />
-                            </svg>
-                            <span class="overflow-hidden whitespace-nowrap">{{ item.cod_payment }}</span>
-                        </div>
-                        </Link>
-                    </MyTransition>
-                    <MyTransition v-for="item in paymentAlarms7" :key="item.id" class="ml-4"
-                        :transitiondemonstration="paymentPorVencer">
-                        <Link class="w-full flex items-center" :href="route('payment.index')">
-                        <div class="flex items-center"> <!-- Contenedor del ícono y el título -->
-                            <svg class="w-4 h-4 mr-2 text-yellow-600 dark:text-red" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path
-                                    d="M15.133 10.632v-1.8a5.407 5.407 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V1.1a1 1 0 0 0-2 0v2.364a.944.944 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C4.867 13.018 3 13.614 3 14.807 3 15.4 3 16 3.538 16h12.924C17 16 17 15.4 17 14.807c0-1.193-1.867-1.789-1.867-4.175Zm-13.267-.8a1 1 0 0 1-1-1 9.424 9.424 0 0 1 2.517-6.39A1.001 1.001 0 1 1 4.854 3.8a7.431 7.431 0 0 0-1.988 5.037 1 1 0 0 1-1 .995Zm16.268 0a1 1 0 0 1-1-1A7.431 7.431 0 0 0 15.146 3.8a1 1 0 0 1 1.471-1.354 9.425 9.425 0 0 1 2.517 6.391 1 1 0 0 1-1 .995ZM6.823 17a3.453 3.453 0 0 0 6.354 0H6.823Z" />
-                            </svg>
-                            <span class="overflow-hidden whitespace-nowrap">{{ item.cod_payment }}</span>
-                        </div>
-                        </Link>
-                    </MyTransition>
-                </template>
+                
                 <MyTransition :transitiondemonstration="showingShoppingArea">
                     <Link class="w-full" :href="route('purchaseorders.history')">Compras Completadas</Link>
                 </MyTransition>
@@ -494,6 +458,44 @@ Usuarios
                 <MyTransition :transitiondemonstration="showingFinance">
                     <Link class="w-full" :href="route('deposits.index')">Depósitos</Link>
                 </MyTransition>
+
+                <MyTransition :transitiondemonstration="showingFinance">
+                    <div class="relative">
+                        <button @click="tooglePayment"><span v-if="paymentAlarms3.length + paymentAlarms7.length > 0"
+                                class="absolute top-0 right-0 bg-red-500 text-white rounded-full h-6 w-6 flex items-center justify-center text-xs leading-4">
+                                {{ paymentAlarms3.length + paymentAlarms7.length }}</span>
+                        </button>
+                        <Link class="w-full" :href="route('payment.index')">Pagos OC</Link>
+                    </div>
+                </MyTransition>
+                <template v-if="paymentAlarms3.length !== 0">
+                    <MyTransition v-for="item in paymentAlarms3" :key="item.id" class="ml-4"
+                        :transitiondemonstration="paymentPorVencer">
+                        <Link class="w-full flex items-center" :href="route('payment.index')">
+                        <div class="flex items-center"> <!-- Contenedor del ícono y el título -->
+                            <svg class="w-4 h-4 mr-2 text-red-600 dark:text-red" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path
+                                    d="M15.133 10.632v-1.8a5.407 5.407 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V1.1a1 1 0 0 0-2 0v2.364a.944.944 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C4.867 13.018 3 13.614 3 14.807 3 15.4 3 16 3.538 16h12.924C17 16 17 15.4 17 14.807c0-1.193-1.867-1.789-1.867-4.175Zm-13.267-.8a1 1 0 0 1-1-1 9.424 9.424 0 0 1 2.517-6.39A1.001 1.001 0 1 1 4.854 3.8a7.431 7.431 0 0 0-1.988 5.037 1 1 0 0 1-1 .995Zm16.268 0a1 1 0 0 1-1-1A7.431 7.431 0 0 0 15.146 3.8a1 1 0 0 1 1.471-1.354 9.425 9.425 0 0 1 2.517 6.391 1 1 0 0 1-1 .995ZM6.823 17a3.453 3.453 0 0 0 6.354 0H6.823Z" />
+                            </svg>
+                            <span class="overflow-hidden whitespace-nowrap">{{ item.cod_payment }}</span>
+                        </div>
+                        </Link>
+                    </MyTransition>
+                    <MyTransition v-for="item in paymentAlarms7" :key="item.id" class="ml-4"
+                        :transitiondemonstration="paymentPorVencer">
+                        <Link class="w-full flex items-center" :href="route('payment.index')">
+                        <div class="flex items-center"> <!-- Contenedor del ícono y el título -->
+                            <svg class="w-4 h-4 mr-2 text-yellow-600 dark:text-red" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path
+                                    d="M15.133 10.632v-1.8a5.407 5.407 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V1.1a1 1 0 0 0-2 0v2.364a.944.944 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C4.867 13.018 3 13.614 3 14.807 3 15.4 3 16 3.538 16h12.924C17 16 17 15.4 17 14.807c0-1.193-1.867-1.789-1.867-4.175Zm-13.267-.8a1 1 0 0 1-1-1 9.424 9.424 0 0 1 2.517-6.39A1.001 1.001 0 1 1 4.854 3.8a7.431 7.431 0 0 0-1.988 5.037 1 1 0 0 1-1 .995Zm16.268 0a1 1 0 0 1-1-1A7.431 7.431 0 0 0 15.146 3.8a1 1 0 0 1 1.471-1.354 9.425 9.425 0 0 1 2.517 6.391 1 1 0 0 1-1 .995ZM6.823 17a3.453 3.453 0 0 0 6.354 0H6.823Z" />
+                            </svg>
+                            <span class="overflow-hidden whitespace-nowrap">{{ item.cod_payment }}</span>
+                        </div>
+                        </Link>
+                    </MyTransition>
+                </template>
             </template>
 
 
@@ -835,10 +837,10 @@ export default {
         if (this.hasPermission('PurchasingManager') || this.hasPermission('Purchasing')) {
             this.fetchPurchasesRequest();
             this.fetchPurchaseOrderAlarms();
-            this.fetchPaymentsAlarm();
         }
         if (this.hasPermission('FinanceManager') || this.hasPermission('Finance')) {
             this.fetchFinancePurchases();
+            this.fetchPaymentsAlarm();
         }
         if (this.hasPermission('DocumentGestion')){
             this.fetchArchiveRequest();
@@ -857,13 +859,13 @@ export default {
             if (this.hasPermission('PurchasingManager') || this.hasPermission('Purchasing')) {
                 this.fetchPurchasesRequest();
                 this.fetchPurchaseOrderAlarms();
-                this.fetchPaymentsAlarm();
             }
             if (this.hasPermission('FinanceManager') || this.hasPermission('Finance')) {
                 this.fetchFinancePurchases();
+                this.fetchPaymentsAlarm();
             }
             if (this.hasPermission('DocumentGestion')){
-            this.fetchArchiveRequest();
+                this.fetchArchiveRequest();
             }
         }, 60000);
     },
