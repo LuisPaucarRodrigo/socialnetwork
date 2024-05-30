7b5add59a75446f507e901dddab7ba1b9097099b
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-05-30 15:10:41 -0500

    seeder completed

--- routes/snsot_route.php
+++ routes/snsot_route.php
@@ -13,18 +13,17 @@ Route::middleware('permission:SocialNetworkProgramation')->group(function () {
     Route::post('/social-network/sot-programation-update/{sot_id}', [SotController::class, 'sot_programation_update'])->name('socialnetwork.sot.programation.update');
     
 
+    
+});
+
+Route::middleware('permission:SocialNetworkOperation')->group(function () {
     Route::get('/social-network/sot-operation', [SotController::class, 'sot_operation'])->name('socialnetwork.sot.operation');
     Route::post('/social-network/sot-operation-store', [SotController::class, 'sot_operation_store'])->name('socialnetwork.sot.operation.store');
     Route::put('/social-network/sot-operation-update/{sot_operation_id}', [SotController::class, 'sot_operation_update'])->name('socialnetwork.sot.operation.update');
-});
-
-Route::middleware('permission:SocialNetworkLiquidation')->group(function () {
-    Route::get('/social-network/sot-liquidation', [SotController::class, 'sot_liquidation'])->name('socialnetwork.sot.liquidation');
-    Route::post('/social-network/sot-liquidation-store', [SotController::class, 'sot_liquidation_store'])->name('socialnetwork.sot.liquidation.store');
-    Route::put('/social-network/sot-programation-update/{sot_liquidation_id}', [SotController::class, 'sot_liquidation_update'])->name('socialnetwork.sot.liquidation.update');
 }); 
 
-Route::middleware('permission:SocialNetworkOperation')->group(function () {
+
+Route::middleware('permission:SocialNetworkLiquidation')->group(function () {
     Route::get('/social-network/sot-liquidation', [SotController::class, 'sot_liquidation'])->name('socialnetwork.sot.liquidation');
     Route::post('/social-network/sot-liquidation-store', [SotController::class, 'sot_liquidation_store'])->name('socialnetwork.sot.liquidation.store');
     Route::put('/social-network/sot-programation-update/{sot_liquidation_id}', [SotController::class, 'sot_liquidation_update'])->name('socialnetwork.sot.liquidation.update');
