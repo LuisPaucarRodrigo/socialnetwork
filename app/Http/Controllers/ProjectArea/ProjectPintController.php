2851215c511668c71917766d0ba32c15153e4a6c
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-06-13 15:51:09 -0500

    prototype create project template pint

--- app/Http/Controllers/ProjectArea/ProjectPintController.php
+++ app/Http/Controllers/ProjectArea/ProjectPintController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\ProjectArea;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ProjectPintController extends Controller
 {
-    public function pintCreateProject() {
-        
+    public function pint_create_project() {
+        return Inertia::render('ProjectArea/Preproject/CreateProjectPint');
     }
 }
