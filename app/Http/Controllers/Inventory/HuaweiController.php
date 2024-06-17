5f455abc67bb0f2223efcb307b5b015e8d11d3ce
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-06-17 17:00:16 -0500

    all fine

--- app/Http/Controllers/Inventory/HuaweiController.php
+++ app/Http/Controllers/Inventory/HuaweiController.php
@@ -3,9 +3,65 @@
 namespace App\Http\Controllers\Inventory;
 
 use App\Http\Controllers\Controller;
+use App\Models\HuaweiProduct;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+use Maatwebsite\Excel\HeadingRowImport;
 
 class HuaweiController extends Controller
 {
     //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public function store (Request $request) {
+        $request->validate([
+            'file' => 'required|mimes:xls,xlsx',
+        ]);
+        $file = $request->file('file');
+        $importData = Excel::toArray([], $file);
+
+        foreach ($importData[0] as $row) {
+
+            $an_sanitize = $this->sanitize_text($row[0]);
+
+            // $annex1 = 
+            // if ($an_sanitize === '') {
+
+            // } else {
+                
+            // }
+
+            HuaweiProduct::create([
+                'nombre' => $row[0],
+                'apellido' => $row[1],
+            ]);
+        }
+
+        return back()->with('success', 'Empleados importados con éxito.');
+
+
+    }
+
+
+
+
+    function sanitize_text($text) {
+        $text = strtolower($text);
+        $text = preg_replace('/["\'(),\s]/', '', $text);
+        return $text;
+    }
+    
 }
