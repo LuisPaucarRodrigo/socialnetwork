8237c9f03f87fc060136a3c76ef36c725bc28b4e
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-06-26 15:02:46 -0500

    all fine

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<link rel="icon" href="/image/LOGO CCIP.png" type="image/png">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
