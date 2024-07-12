5ece227f3ce5ded6341854284bcd8235c4eb0100
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2024-07-12 13:18:32 -0500

    cicsa_process

---
+++ app/Models/CicsaFeasibilityMaterial.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CicsaFeasibilityMaterial extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cicsa_feasibility_materials';
+
+    protected $fillable = [
+        'name',
+        'unit',
+        'quantity',
+        'cicsa_feasibility_id'
+    ];
+
+    public function cicsa_feasibility ()
+    {
+        return $this->belongsTo(CicsaFeasibility::class, 'cicsa_feasibility_id');
+    }
+}
