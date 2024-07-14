97abe56b941b8c56081b3ec913fdf7109a6552c4
LuisPaucarRodrigo <luis.paucar.ro@tecsup.edu.pe>
2024-07-13 20:33:35 -0500

    juan traca

--- resources/js/Pages/Cicsa/CicsaMaterial.vue
+++ resources/js/Pages/Cicsa/CicsaMaterial.vue
@@ -38,7 +38,7 @@
                             </th>
                             <th
                                 class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-center text-xs font-semibold uppercase tracking-wider text-gray-600">
-                                Nombre Usuario
+                                Encargado
                             </th>
                             <th
                                 class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-center text-xs font-semibold uppercase tracking-wider text-gray-600">
