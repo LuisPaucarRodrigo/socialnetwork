70aab6354ea72359a94006e5e7bf5d166347dcb6
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-07-19 06:26:52 -0500

    download complete

--- app/Http/Controllers/ProjectArea/ProjectDocumentController.php
+++ app/Http/Controllers/ProjectArea/ProjectDocumentController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use ZipArchive;
+use Response;
 
 class ProjectDocumentController extends Controller
 {
@@ -60,9 +62,15 @@ class ProjectDocumentController extends Controller
         return redirect()->back();
     }
 
-    public function project_doc_folder_delete (Request $request){
+    public function project_doc_delete (Request $request){
         $path = $request->input('path');
-        $this->deleteFolder($path);
+        $type = $request->input('type');
+        if ($type === 'folder'){
+            $this->deleteFolder($path);
+        } 
+        if ($type === 'archive'){
+            $this->deleteArchive($path);
+        }
         return redirect()->back();
     }
 
@@ -104,6 +112,12 @@ class ProjectDocumentController extends Controller
         }
     }
 
+    
+    public function createArchive ($file, $path){
+        $filename = $file->getClientOriginalName();
+        $filePath = $file->storeAs($path, $filename);
+    }
+
     public function deleteFolder($path){
         if (Storage::exists($path)) {
             Storage::deleteDirectory($path);
@@ -111,10 +125,84 @@ class ProjectDocumentController extends Controller
     }
 
 
-    public function createArchive ($file, $path){
-        $filename = $file->getClientOriginalName();
-        $filePath = $file->storeAs($path, $filename);
+    public function deleteArchive($path){
+        if (Storage::disk('local')->exists($path)) {
+            Storage::disk('local')->delete($path);
+        } else {
+            return response()->json(['message' => 'Archivo no encontrado'], 404);
+        }
     }
+
+
+
+
+
+
+    public function project_doc_download(Request $request)
+    {
+        $path = $request->input('path');
+        $storagePath = storage_path('app/' . $path);
+
+        if (is_file($storagePath)) {
+            ob_end_clean();
+            return $this->downloadFile($path);
+        } elseif (is_dir($storagePath)) {
+            ob_end_clean();
+            return $this->downloadFolderAsZip($path);
+        } else {
+            return abort(404, 'El recurso solicitado no existe.');
+        }
+    }
+
+    private function downloadFile($path)
+    {
+        $filePath = storage_path('app/' . $path);
+
+        if (!file_exists($filePath)) {
+            return abort(404, 'El archivo no existe.');
+        }
+
+        return response()->download($filePath);
+    }
+
+    private function downloadFolderAsZip($folderPath)
+    {
+        $zip = new ZipArchive();
+        $zipFileName = storage_path('app/' . basename($folderPath) . '.zip');
+
+        if ($zip->open($zipFileName, ZipArchive::CREATE | ZipArchive::OVERWRITE) !== true) {
+            return abort(500, 'No se pudo crear el archivo zip.');
+        }
+
+        $this->addFolderToZip(storage_path('app/' . $folderPath), $zip);
+
+        $zip->close();
+
+        return response()->download($zipFileName)->deleteFileAfterSend(true);
+    }
+
+    private function addFolderToZip($folder, ZipArchive $zip, $parentFolder = '')
+    {
+        $files = scandir($folder);
+
+        foreach ($files as $file) {
+            if ($file == '.' || $file == '..') {
+                continue;
+            }
+
+            $filePath = $folder . '/' . $file;
+            $localPath = $parentFolder ? $parentFolder . '/' . $file : $file;
+
+            if (is_dir($filePath)) {
+                $zip->addEmptyDir($localPath);
+                $this->addFolderToZip($filePath, $zip, $localPath);
+            } else {
+                $zip->addFile($filePath, $localPath);
+            }
+        }
+    }
+
+
     
 
 }
--- resources/js/Pages/ProjectArea/ProjectDocument/ProjectFolder.vue
+++ resources/js/Pages/ProjectArea/ProjectDocument/ProjectFolder.vue
@@ -8,13 +8,15 @@
             {{ currentPath }}
         </template>
         <div class="min-w-full rounded-lg shadow">
-            <div class="flex justify-between">
+            <div class="flex space-x-4">
                 <PrimaryButton 
                     @click="openAddFoldermodal" type="button">
                     + Agregar
                 </PrimaryButton>
+                
 
-                <div class="flex items-center mt-4 sm:mt-0">
+
+                <!-- <div class="flex items-center mt-4 sm:mt-0">
                     <form 
                         @submit.prevent="search"
                         class="flex items-center w-full sm:w-auto"
@@ -30,7 +32,7 @@
                             </svg>
                         </button>
                     </form>
-                </div>
+                </div> -->
 
             </div>
 
@@ -92,7 +94,7 @@
                                             d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                     </svg>
                                     </Link> -->
-                                    <a type="button" :href="'#'">
+                                    <a type="button" :href="route('project.folder.download', {path: item.path})">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                             stroke-width="1.5" stroke="green" :class="`w-6 h-6`">
                                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -180,12 +182,21 @@
         <Modal :show="showDeleteFolderModal" @close="closeDeleteFolderModal">
             <div class="p-6">
                 <h2 class="text-lg font-medium text-gray-900 mb-2">
-                    Eliminación de Carpeta
+                    {{ itemToDelete.type === 'folder' ? 'Eliminación de Carpeta' :
+                         itemToDelete.type === 'archive' && 'Eliminación de Archivo'
+                    }}
                 </h2>
                 <InputLabel class="mb-5">
-                    ¿Estás seguro de querer eliminar toda la carpeta?, esta acción eliminará también las subcarpetas y
-                    archivos
-                    de esta. La acción es irreversible.
+                    <template v-if="itemToDelete.type === 'folder'">
+                        ¿Estás seguro de querer eliminar toda la carpeta 
+                        <span class="mx-1 font-bold text-lg">{{ itemToDelete.name }}</span>?
+                        Esta acción eliminará también las subcarpetas y archivos de esta. La acción es irreversible.
+                    </template>
+                    <template v-else-if="itemToDelete.type === 'archive'">
+                        ¿Estás seguro de querer eliminar el archivo 
+                        <span class=" mx-1 font-bold text-lg">{{ itemToDelete.name }}</span>?
+                        La acción es irreversible.
+                    </template>
                 </InputLabel>
                 <div class="mt-6 flex justify-end">
                     <SecondaryButton @click="closeDeleteFolderModal"> Cancelar </SecondaryButton>
@@ -286,17 +297,17 @@ function submit() {
 
 //--------- Delete Folder ---------//
 const showDeleteFolderModal = ref(false)
-const folderToDelete = ref(null)
+const itemToDelete = ref(null)
 function openDeleteFolderModal(item) {
-    folderToDelete.value = item
+    itemToDelete.value = item
     showDeleteFolderModal.value = true
 }
 function closeDeleteFolderModal() {
-    folderToDelete.value = null
+    itemToDelete.value = null
     showDeleteFolderModal.value = false
 }
 function deleteFolder() {
-    router.post(route('project.folder.delete'),  { path: folderToDelete.value.path }, {
+    router.post(route('project.folder.delete'),  { path: itemToDelete.value.path, type: itemToDelete.value.type }, {
         onSuccess: () => {
             closeDeleteFolderModal()
         }, onError: (e) => {
