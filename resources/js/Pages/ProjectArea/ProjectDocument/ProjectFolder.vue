15baa68f3031cc7729b76056e4a001337ee885c9
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-07-19 06:35:17 -0500

    folders in projects completed

--- resources/js/Pages/ProjectArea/ProjectDocument/ProjectFolder.vue
+++ resources/js/Pages/ProjectArea/ProjectDocument/ProjectFolder.vue
@@ -68,19 +68,26 @@
                             <td class="border-b border-gray-200 bg-white  text-sm">
                                 <!-- for button instead of Link -->
                                 <!-- @click="() => router.visit(route('documment.management.folders', { folder_id: item.item_db.id }))" -->
-                                <Link :href="route('project.document.index', { path: item.path })"
+                                <Link v-if="item.type === 'folder'"
+                                    :href="route('project.document.index', { path: item.path })"
                                     class="inline-block w-full h-full text-left px-5 py-5 text-gray-900 whitespace-nowrap font-bold hover:cursor-pointer hover:text-indigo-600 tracking-widest text-base hover:opacity-70 hover:underline">
                                 <div class="flex space-x-8 items-center w-full mr-8 xl:mr-0">
                                     <FolderIcon v-if="item.type === 'folder'" class="h-10 w-10" />
-                                    <DocumentTextIcon v-if="item.type === 'archive'" class="h-9 w-9" />
                                     <p>
                                         {{ item.name }}
                                     </p>
                                 </div>
-
                                 </Link>
-
-
+                                <a v-if="item.type === 'archive'"
+                                    :href="route('project.folder.download', { path: item.path })"
+                                    class="inline-block w-full h-full text-left px-5 py-5 text-gray-900 whitespace-nowrap font-bold hover:cursor-pointer hover:text-indigo-600 tracking-widest text-base hover:opacity-70 hover:underline">
+                                    <div class="flex space-x-8 items-center w-full mr-8 xl:mr-0">
+                                        <DocumentTextIcon class="h-9 w-9" />
+                                        <p>
+                                            {{ item.name }}
+                                        </p>
+                                    </div>
+                                </a>
                             </td>
                             <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
 
@@ -100,8 +107,15 @@
                                     </svg>
                                     </Link> -->
                                     <a type="button" :href="route('project.folder.download', { path: item.path })">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="green" :class="`w-6 h-6`">
+                                        <svg v-if="item.type === 'folder'" xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green"
+                                            class="w-6 h-6 text-white">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                                        </svg>
+                                        <svg v-if="item.type === 'archive'" xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green"
+                                            :class="`w-6 h-6`">
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
                                         </svg>
