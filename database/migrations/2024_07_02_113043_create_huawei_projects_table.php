d0125d1294f7bcb019b314a9701d016da330707d
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2024-07-31 17:39:25 -0500

    earningAndExport

--- database/migrations/2024_07_02_113043_create_huawei_projects_table.php
+++ database/migrations/2024_07_02_113043_create_huawei_projects_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->foreignId('huawei_site_id')->constrained('huawei_sites')->onDelete('cascade');
             $table->string('description')->nullable();
-            $table->string('ot');
+            $table->string('ot')->nullable();
             $table->string('pre_report')->nullable();
             $table->double('initial_amount')->nullable();
             $table->string('assigned_diu');
---
+++ resources/views/Export/HuaweiInventoryExport.blade.php
@@ -0,0 +1,54 @@
+<table>
+    <thead>
+        <tr>
+            <td>N°</td>
+            <td>FECHA</td>
+            <td>CODSAP</td>
+            <td>DESCRIPCIÓN</td>
+            <td>SERIE</td>
+            <td>CONTRATA</td>
+            <td>SITE DONDE SE SOLICITÓ LOS EQUIPOS EN PAP</td>
+            <td>ESTATUS GENERAL</td>
+            <td>OBSERVACIÓN</td>
+            <td>FECHA DE INSTALACIÓN</td>
+            <td>DIU DEL SITIO ASIGNADO</td>
+            <td>VALOR MONETARIO</td>
+            <td>EQUIPOS TRASPASADOS</td>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($equipments as $index => $item)
+        <tr>
+            <td>{{ $index + 1 }}</td>
+            <td>{{ $item->huawei_entry_detail->huawei_entry->entry_date }}</td>
+            <td>{{ $item->huawei_equipment->claro_code }}</td>
+            <td>{{ $item->huawei_equipment->name }}</td>
+            <td>{{ $item->serie_number }}</td>
+            <td>CCIP</td>
+            <td>{{ $item->huawei_entry_detail->latest_huawei_project_resource ? $item->huawei_entry_detail->latest_huawei_project_resource->huawei_project->huawei_site->name : '' }}</td>
+            <td>{{ $item->huawei_entry_detail->state }}</td>
+            <td>
+                @php
+                    $observation = $item->huawei_entry_detail->observation;
+                    $parts = explode('/', $observation);
+                    // Tomar la primera parte y recortar espacios en blanco
+                    $result = isset($parts[0]) ? trim($parts[0]) : '';
+                @endphp
+                {{ $result }}
+            </td>
+            <td>{{ $item->huawei_entry_detail->latest_huawei_project_resource ? $item->huawei_entry_detail->latest_huawei_project_resource->created_at : '' }}</td>
+            <td>{{ $item->huawei_entry_detail->assigned_diu }}</td>
+            <td>{{ $item->huawei_entry_detail->unit_price }}</td>
+            <td>
+                @php
+                    $observation = $item->huawei_entry_detail->observation;
+                    $parts = explode('/', $observation);
+                    // Si hay al menos dos partes, tomar la segunda parte y recortar espacios en blanco
+                    $result = isset($parts[1]) ? trim($parts[1]) : '';
+                @endphp
+                {{ $result }}
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
