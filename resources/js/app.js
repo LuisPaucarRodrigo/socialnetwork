5b86df84390a33d9bf56c55e0c6fe8c0d39e2cf5
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-08-02 02:15:42 -0500

    tooltips in buttons

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { initFlowbite } from 'flowbite';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -20,10 +21,18 @@ createInertiaApp({
             }
         });
 
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue, Ziggy)
-            .mount(el);
+         // Crear una instancia de la aplicación Vue
+         const app = createApp({
+            render: () => h(App, props),
+            mounted() {
+                // Inicializar Flowbite aquí
+                initFlowbite();
+            }
+        });
+
+        app.use(plugin);
+        app.use(ZiggyVue, Ziggy);
+        app.mount(el);
     },
     progress: {
         color: '#497DFF',
