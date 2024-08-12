071e2176ef1006a09036de900513d1fc5d5e16a9
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-08-12 16:09:05 -0500

    all fine

--- app/Models/ChecklistEpp.php
+++ app/Models/ChecklistEpp.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class ChecklistEpp extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'helmet',
+        'chin_strap',
+        'windbreaker',
+        'vest',
+        'safety_shoes',
+        'tshirt_ls',
+        'shirt_ls',
+        'jeans',
+        'coveralls',
+        'jacket',
+        'dark_glasses',
+        'clear_glasses',
+        'overglasses',
+        'dust_mask',
+        'earplugs',
+        'latex_oil_gloves',
+        'nitrile_oil_gloves',
+        'split_leather_gloves',
+        'precision_gloves',
+        'cut_resistant_gloves',
+        'double_lanyard',
+        'harness',
+        'positioning_lanyard',
+        'carabiners',
+        'ascenders',
+        'sunscreen',
+    ];
 }
--- database/migrations/2024_08_12_154654_create_checklist_dailytoolkits_table.php
+++ database/migrations/2024_08_12_154654_create_checklist_dailytoolkits_table.php
@@ -13,6 +13,32 @@ return new class extends Migration
     {
         Schema::create('checklist_dailytoolkits', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->string('personal_2');
+            $table->string('zone');
+
+            $table->string('power_meter');
+            $table->string('ammeter_clamp');
+            $table->string('cutting_pliers');
+            $table->string('nose_pliers');
+            $table->string('universal_pliers');
+            $table->string('tape');
+            $table->string('cutter');
+            $table->string('knob_driver');
+            $table->string('screwdriver_set');
+            $table->string('allenkeys_set');
+            $table->string('thor_screwboard');
+            $table->string('helmet_flashlight');
+            $table->string('freanch_key');
+            $table->string('pyrometer');
+            $table->string('laptop');
+            $table->string('console_cables');
+            $table->string('network_adapter');
+
+
+            $table->text('observations');
+
+
             $table->timestamps();
         });
     }
