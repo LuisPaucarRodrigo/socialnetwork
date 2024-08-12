46413356d3d3985a5cbf9e60135dbd5683d01344
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-08-12 16:31:32 -0500

    all fine

---
+++ app/Http/Controllers/ProjectArea/ChecklistsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ProjectArea;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ChecklistsController extends Controller
+{
+    //
+}
