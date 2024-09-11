801701a11331d1187bcdcb15074873513582f786
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-09-11 16:41:35 -0500

    all fine

--- resources/js/Pages/ProjectArea/PreProject/ImageReport/index.vue
+++ resources/js/Pages/ProjectArea/PreProject/ImageReport/index.vue
@@ -47,7 +47,7 @@
                                 role="tooltip"
                                 class="absolute z-50 invisible inline-block px-2 py-2 text-xs font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
                             >
-                                Actualizar porque si puedo aparecer
+                                {{ image.description }}
                                 <div class="tooltip-arrow" data-popper-arrow></div>
                             </div>
                           
