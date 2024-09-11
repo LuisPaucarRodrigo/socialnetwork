038a56aea17f370c6da7f9e63f4f1ef78be8f745
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-09-11 07:15:16 -0500

    all fine

--- resources/js/Pages/ProjectArea/ProjectManagement/AdditionalCosts.vue
+++ resources/js/Pages/ProjectArea/ProjectManagement/AdditionalCosts.vue
@@ -153,8 +153,7 @@
                     >
                         <svg
                             fill="#000000"
-                            width="800px"
-                            height="800px"
+                            class="size-5"
                             viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg"
                         >
