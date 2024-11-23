1124b7ea1ebca127be0952da31f770dcf16eff6c
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2024-11-23 12:41:04 -0500

    all fine

--- app/Http/Controllers/DocumentSpreedSheetController.php
+++ app/Http/Controllers/DocumentSpreedSheetController.php
@@ -21,7 +21,7 @@ class DocumentSpreedSheetController extends Controller
         $employees = Employee::with([
             'document_registers',
             'contract:id,state,employee_id,hire_date,discount_sctr,pension_id',
-            ])
+        ])
             ->select(
                 'id',
                 'name',
@@ -64,7 +64,7 @@ class DocumentSpreedSheetController extends Controller
             });
         $e_employees = ExternalEmployee::with([
             'document_registers',
-            ])
+        ])
             ->select(
                 'id',
                 'name',
@@ -99,7 +99,7 @@ class DocumentSpreedSheetController extends Controller
                         ];
                     }
                 );
-                $emp->contract = ['state'=>'External'];
+                $emp->contract = ['state' => 'External'];
                 $emp->setRelation('document_registers', $formattedDr);
                 return $emp;
             });
@@ -115,113 +115,113 @@ class DocumentSpreedSheetController extends Controller
     }
 
 
-    public function employee_document_alarms ($emp_id) {
+    public function employee_document_alarms($emp_id) 
+    {
         $employee = Employee::with([
             'document_registers',
             'contract:id,state,employee_id,hire_date,discount_sctr,pension_id',
         ])
-        ->select(
-            'id',
-            'name',
-            'lastname',
-            'phone1',
-            'email',
-            'email_company',
-            'dni',
-            'l_policy',
-            'sctr_exp_date',
-            'policy_exp_date',
-        )
-        ->find($emp_id);
-    
-    if ($employee) {
-        $formattedDr = $employee->document_registers->mapWithKeys(function ($dr) {
-            return [
-                $dr->subdivision_id => [
-                    'id' => $dr->id,
-                    'document_id' => $dr->document_id,
-                    'employee_id' => $dr->employee_id,
-                    'e_employee_id' => $dr->e_employee_id,
-                    'exp_date' => $dr->exp_date,
-                    'state' => $dr->state,
-                    'observations' => $dr->observations,
-                    'sync_status' => $dr->sync_status,
-                    'display' => $dr->display,
-                ],
-            ];
-        });
-    
-        $employee->setRelation('document_registers', $formattedDr);
-    }
-    return Inertia::render('HumanResource/DocumentSpreedSheet/EmployeeDocumentAlarms', ['employee'=>$employee]);
-    
+            ->select(
+                'id',
+                'name',
+                'lastname',
+                'phone1',
+                'email',
+                'email_company',
+                'dni',
+                'l_policy',
+                'sctr_exp_date',
+                'policy_exp_date',
+            )
+            ->find($emp_id);
 
+        if ($employee) {
+            $formattedDr = $employee->document_registers->mapWithKeys(function ($dr) {
+                return [
+                    $dr->subdivision_id => [
+                        'id' => $dr->id,
+                        'document_id' => $dr->document_id,
+                        'employee_id' => $dr->employee_id,
+                        'e_employee_id' => $dr->e_employee_id,
+                        'exp_date' => $dr->exp_date,
+                        'state' => $dr->state,
+                        'observations' => $dr->observations,
+                        'sync_status' => $dr->sync_status,
+                        'display' => $dr->display,
+                    ],
+                ];
+            });
 
-
-
-
-
-
-        
+            $employee->setRelation('document_registers', $formattedDr);
+        }
+        $sections = DocumentSection::with('subdivisions')->where('id', '<=', 10)->get();
+        return Inertia::render('HumanResource/DocumentSpreedSheet/EmployeeDocumentAlarms', [
+            'employee' => $employee,
+            'sections' => $sections,
+        ]);
     }
 
 
 
 
-    public function store(DocumentRegisterRequest $request, $dr_id=null) {
+    public function store(DocumentRegisterRequest $request, $dr_id = null)
+    {
         $data = $request->validated();
         $item = DocumentRegister::find($dr_id);
-        if ($item){
+        if ($item) {
             $item->update($data);
         } else {
             $item = DocumentRegister::create($data);
         }
         $docItem = Document::find($item->document_id);
-        if ($docItem && $docItem->exp_date === null){
-            $docItem->update(['exp_date'=>$item->exp_date]);
+        if ($docItem && $docItem->exp_date === null) {
+            $docItem->update(['exp_date' => $item->exp_date]);
         }
 
         $item->without('document');
-        return response()->json([$item->subdivision_id=>$item], 200);
+        return response()->json([$item->subdivision_id => $item], 200);
     }
 
 
-    public function destroy ($dr_id=null){
+    public function destroy($dr_id = null)
+    {
         $item = DocumentRegister::find($dr_id);
-        if ($item->document_id){
-            return response()->json(['msg'=>'El registro del documento ya esta asociado a un archivo en el aplicativo'], 200);
+        if ($item->document_id) {
+            return response()->json(['msg' => 'El registro del documento ya esta asociado a un archivo en el aplicativo'], 200);
         } else {
             $item->delete();
-            return response()->json(['msg'=>'Eliminado'], 200);
+            return response()->json(['msg' => 'Eliminado'], 200);
         }
     }
 
 
-    public function insurance_exp_date (InsuranceExpDateRequest  $request){
+    public function insurance_exp_date(InsuranceExpDateRequest $request)
+    {
         $data = $request->validated();
-        if($data['title']==='SCTR'){
+        if ($data['title'] === 'SCTR') {
             Employee::whereHas('contract', function ($query) {
                 $query->where('discount_sctr', 1);
-            })->update(['sctr_exp_date'=>$data['exp_date']]);
-            ExternalEmployee::where('sctr', 1)->update(['sctr_exp_date'=>$data['exp_date']]);
+            })->update(['sctr_exp_date' => $data['exp_date']]);
+            ExternalEmployee::where('sctr', 1)->update(['sctr_exp_date' => $data['exp_date']]);
         }
-        if($data['title']==='Póliza'){
-            Employee::where('l_policy', '1')->update(['policy_exp_date'=>$data['exp_date']]);
-            ExternalEmployee::where('l_policy', 1)->update(['sctr_exp_date'=>$data['exp_date']]);
+        if ($data['title'] === 'Póliza') {
+            Employee::where('l_policy', '1')->update(['policy_exp_date' => $data['exp_date']]);
+            ExternalEmployee::where('l_policy', 1)->update(['sctr_exp_date' => $data['exp_date']]);
         }
-        return response()->json(['msg'=>'success'], 200);
+        return response()->json(['msg' => 'success'], 200);
     }
 
 
 
-    public function employeesDocumentAlarms() {
-        $employees = Employee::orderBy('lastname')->get()->filter(function($item){
+    public function employeesDocumentAlarms()
+    {
+        $employees = Employee::orderBy('lastname')->get()->filter(function ($item) {
             return $item->documents_about_to_expire > 0;
         })->values()->all();
-        $e_employees = ExternalEmployee::orderBy('lastname')->get()->filter(function($item){    
+        $e_employees = ExternalEmployee::orderBy('lastname')->get()->filter(function ($item) {
             return $item->documents_about_to_expire > 0;
         })->values()->all();
-        $employees = array_merge($employees ,$e_employees);
+        $employees = array_merge($employees, $e_employees);
         return response()->json($employees, 200);
     }
 
@@ -238,7 +238,7 @@ class DocumentSpreedSheetController extends Controller
     //                     'e_employee_id'=> $dr->e_employee_id,
     //                 ]);
     //             }
-                
+
     //         }
     //         DB::commit();
     //     } catch (e) {
@@ -279,7 +279,7 @@ class DocumentSpreedSheetController extends Controller
     //         DB::rollBack();
     //         dd('errrorr creando doc registers');
     //     }
-        
+
     // }
 
 }
