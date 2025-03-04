92f94536d29d24539a7997442c13e8a2bfb1bbe8
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2025-03-04 07:14:00 -0500

    all fine

--- resources/js/Pages/Finance/AccountStatement/AccountStatement.vue
+++ resources/js/Pages/Finance/AccountStatement/AccountStatement.vue
@@ -928,7 +928,7 @@
                                         type="file"
                                         v-model="importForm.excel_file"
                                         id="excel_file"
-                                        accept=".xlsx"
+                                        accept=".xlsx, .csv"
                                         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     />
                                     <InputError
