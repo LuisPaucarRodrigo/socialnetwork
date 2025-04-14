52071a90d20fd84ad1b8697ab3757210d14f44b9
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2025-04-13 21:21:07 -0500

    all fine

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,6 +11,7 @@ use App\Enums\Permissions\UserRolePermissions;
 use App\Http\Requests\ProfileUpdateRequest;
 use App\Models\Permission;
 use App\Models\Project;
+use App\Models\CicsaAssignation;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -24,10 +25,38 @@ use Inertia\Response;
 
 class ProfileController extends Controller
 {
-    public function allFine(){
-        $data = Project::select('description')->with('cicsa_assignation')->where('cost_center_id', 1)->get();
-        
-        return response()->json($data);
+    public function allFine()
+    {
+        // $data = Project::with('cicsa_assignation', 'preproject.customer')
+        //     ->where('cost_center_id', 1)
+        //     ->whereIn('id', [368, 424, 451])
+        //     ->get();
+
+        // DB::beginTransaction();
+        // try {
+        //     foreach ($data as $project) {
+        //         $preproject = $project->preproject;
+        //         CicsaAssignation::create([
+        //             "assignation_date" => $preproject->date,
+        //             "project_name" => $project->description,
+        //             "customer" => $preproject->customer->business_name,
+        //             "project_code" => $preproject->code,
+        //             "cpe" => $preproject->cpe,
+        //             "zone" => 'Arequipa',
+        //             "zone2" => null,
+        //             "manager" => 'Nikol Sheyla Rondón Neyra',
+        //             "user_name" => 'Nikol Sheyla Rondón Neyra',
+        //             "user_id" => 6,
+        //             "project_id" => $project->id,
+        //         ]);
+        //     }
+        //     DB::commit();
+        //     return response()->json('all fine');
+        // } catch (Exception $e) {
+        //     DB::rollBack();
+        //     return $e->getMessage();
+        // }
+
     }
 
     /**
