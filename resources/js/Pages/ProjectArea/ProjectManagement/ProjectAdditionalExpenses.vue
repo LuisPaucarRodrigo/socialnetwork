0bf97508184ad49dbb83ec802f55ec002bf73999
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2025-05-09 10:41:27 -0500

    fix: new render method in chart and expenses

--- resources/js/Pages/ProjectArea/ProjectManagement/ProjectAdditionalExpenses.vue
+++ resources/js/Pages/ProjectArea/ProjectManagement/ProjectAdditionalExpenses.vue
@@ -157,8 +157,7 @@
         <div class="overflow-x-auto h-[85vh]">
             <div class="mb-4">
                 <ChartsAdditionalExpenses 
-                    :acExpensesAmounts="acExpensesAmounts"
-                    :scExpensesAmounts="scExpensesAmounts"
+                    :project_id="Number(project_id)" :reload_flag="reload_flag"
                 />
             </div>
             <table class="w-full">
@@ -510,8 +509,6 @@ const props = defineProps({
     project_id: String,
     fixedOrAdditional: Boolean,
     type: String,
-    acExpensesAmounts: Array,
-    scExpensesAmounts: Array,
     zones:Array,
     expenseType:Array,
     documentsType:Array,
@@ -520,6 +517,7 @@ const props = defineProps({
 });
 
 const expenses = ref({...props.expense});
+const reload_flag = ref(true)
 // const filterMode = ref(false);
 // const subCostCenterZone = ref(null);
 // const subCostCenter = ref(null)
@@ -731,7 +729,7 @@ const submitSwapAPModal = async () => {
             expenses.value = expenses.value.filter((item) => 
             !actionForm.value.ids.includes(item.id));
         }
-
+    reload_flag.value = !reload_flag.value
     actionForm.value.ids = []
 
     closeSwapAPModal();
