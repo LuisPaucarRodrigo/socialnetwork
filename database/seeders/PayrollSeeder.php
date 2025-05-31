e12310a9c5af2f7698df85a4529473479f8c2737
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2025-05-30 19:39:05 -0500

    all fine

--- database/seeders/PayrollSeeder.php
+++ database/seeders/PayrollSeeder.php
@@ -16,8 +16,14 @@ class PayrollSeeder extends Seeder
     public function run(): void
     {
         $incomeParams = [
+            ['code' => '0105', 'concept' => 'Trabajo sobretiempo (H. extras 25%)'],
+            ['code' => '0106', 'concept' => 'Trabajo sobretiempo (H. extras 35%)'],
+            ['code' => '0107', 'concept' => 'Trabajo en feriado o día descando'],
+            ['code' => '0114', 'concept' => 'Vacaciones truncas'],
+            ['code' => '0118', 'concept' => 'Remuneración vacacional'],
             ['code' => '0121', 'concept' => 'Remuneración o jornal básico'],
             ['code' => '0122', 'concept' => 'Remuneración permanente'],
+
             ['code' => '0201', 'concept' => 'Asignación familiar'],
             ['code' => '0306', 'concept' => 'Bonificaciones regulares'],
             ['code' => '0902', 'concept' => 'Bono de productividad'],
@@ -27,30 +33,30 @@ class PayrollSeeder extends Seeder
             ['code' => '0928', 'concept' => 'Devolución retención exceso de imp renta 5ta cat.'],
         ];
         IncomeParam::insert($incomeParams);
-        $discountParams = [
-            ['code' => '0701', 'concept' => 'Adelanto'],
-            ['code' => '0702', 'concept' => 'Cuota sindical'],
-            ['code' => '0705', 'concept' => 'Inasistencias'],
-            ['code' => '0706', 'concept' => 'Otros desc. no deduc. de base imponib.'],
-        ];
-        DiscountParam::insert($discountParams);
-        $taxAndContributions = [
-            ['code' => '0601', 'concept' => 'Comisión AFP porcentual', 'type' => 'employee'],
-            ['code' => '0602', 'concept' => 'Conafovicer', 'type' => 'employee'],
-            ['code' => '0605', 'concept' => 'Renta quinta categoría retenciones', 'type' => 'employee'],
-            ['code' => '0606', 'concept' => 'Prima de seguro AFP', 'type' => 'employee'],
-            ['code' => '0608', 'concept' => 'SPP - aportación obligatoria', 'type' => 'employee'],
-            ['code' => '0611', 'concept' => 'Otras aportaciones trab./pensionis.', 'type' => 'employee'],
+        // $discountParams = [
+        //     ['code' => '0701', 'concept' => 'Adelanto'],
+        //     ['code' => '0702', 'concept' => 'Cuota sindical'],
+        //     ['code' => '0705', 'concept' => 'Inasistencias'],
+        //     ['code' => '0706', 'concept' => 'Otros desc. no deduc. de base imponib.'],
+        // ];
+        // DiscountParam::insert($discountParams);
+        // $taxAndContributions = [
+        //     ['code' => '0601', 'concept' => 'Comisión AFP porcentual', 'type' => 'employee'],
+        //     ['code' => '0602', 'concept' => 'Conafovicer', 'type' => 'employee'],
+        //     ['code' => '0605', 'concept' => 'Renta quinta categoría retenciones', 'type' => 'employee'],
+        //     ['code' => '0606', 'concept' => 'Prima de seguro AFP', 'type' => 'employee'],
+        //     ['code' => '0608', 'concept' => 'SPP - aportación obligatoria', 'type' => 'employee'],
+        //     ['code' => '0611', 'concept' => 'Otras aportaciones trab./pensionis.', 'type' => 'employee'],
 
-            ['code' => '0801', 'concept' => 'SPP - aportación voluntaria.', 'type' => 'employer'],
-            ['code' => '0803', 'concept' => 'Póliza de seguro - D. LEG 688', 'type' => 'employer'],
-            ['code' => '0804', 'concept' => 'Essalud(regular CBSSP agrar/ac)trab', 'type' => 'employer'],
-            ['code' => '0805', 'concept' => 'SCTR pensiones', 'type' => 'employer'],
-            ['code' => '0809', 'concept' => 'Otras aportaciones cargo empleador', 'type' => 'employer'],
-            ['code' => '0810', 'concept' => 'EPS seguro complementario de trab', 'type' => 'employer'],
-            ['code' => '0813', 'concept' => 'ONP - SCTR', 'type' => 'employer'],
-            ['code' => '0814', 'concept' => 'Compañía de seguro - SCTR', 'type' => 'employer'],
-        ];
-        TaxAndContributionParam::insert($taxAndContributions);
+        //     ['code' => '0801', 'concept' => 'SPP - aportación voluntaria.', 'type' => 'employer'],
+        //     ['code' => '0803', 'concept' => 'Póliza de seguro - D. LEG 688', 'type' => 'employer'],
+        //     ['code' => '0804', 'concept' => 'Essalud(regular CBSSP agrar/ac)trab', 'type' => 'employer'],
+        //     ['code' => '0805', 'concept' => 'SCTR pensiones', 'type' => 'employer'],
+        //     ['code' => '0809', 'concept' => 'Otras aportaciones cargo empleador', 'type' => 'employer'],
+        //     ['code' => '0810', 'concept' => 'EPS seguro complementario de trab', 'type' => 'employer'],
+        //     ['code' => '0813', 'concept' => 'ONP - SCTR', 'type' => 'employer'],
+        //     ['code' => '0814', 'concept' => 'Compañía de seguro - SCTR', 'type' => 'employer'],
+        // ];
+        // TaxAndContributionParam::insert($taxAndContributions);
     }
 }
